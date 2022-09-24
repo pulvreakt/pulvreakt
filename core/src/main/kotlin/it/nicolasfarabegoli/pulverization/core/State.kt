dc35ecc458b98c6f6d5f83f74a0a97fd769f55a4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-24 19:33:32 +0200

    feat(state): first implementation of the concept of state

--- core/src/main/kotlin/it/nicolasfarabegoli/pulverization/core/State.kt
+++ core/src/main/kotlin/it/nicolasfarabegoli/pulverization/core/State.kt
@@ -1,3 +1,17 @@
 package it.nicolasfarabegoli.pulverization.core
 
-interface State
+/**
+ * Model the concept of _State_ in the pulverization context.
+ * @param S the type of the state.
+ */
+interface State<S> {
+    /**
+     * The device state.
+     */
+    var state: S
+
+    /**
+     * Update the [state] with a [newState].
+     */
+    fun updateState(newState: S)
+}
