27e8b5efb2572bfce0ee1b31a51615c8a31595ed
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-24 17:14:53 +0200

    feat(behaviour): define behaviour structure

--- core/src/main/kotlin/it/nicolasfarabegoli/pulverization/core/Behaviour.kt
+++ core/src/main/kotlin/it/nicolasfarabegoli/pulverization/core/Behaviour.kt
@@ -1,3 +1,13 @@
 package it.nicolasfarabegoli.pulverization.core
 
-interface Behaviour
+/**
+ * Models a _behaviour_ in the pulverization context.
+ * A [Behaviour] is a pure function of the kind `B(x, e, o) = (x', e', a)`
+ * @param S the type of the state
+ * @param E the type of the export
+ * @param W the type of the sensed value
+ * @param A the type of the actuation to do
+ */
+interface Behaviour<S, E, W, A> {
+    fun invoke(state: S, export: E, sensedValue: W): Triple<S, E, A>
+}
