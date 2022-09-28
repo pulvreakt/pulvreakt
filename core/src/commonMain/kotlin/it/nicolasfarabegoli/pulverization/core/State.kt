1011b0f2f200b63c09b60afcf6ee9532e001d375
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-28 15:40:47 +0200

    docs: add class link

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/core/State.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/core/State.kt
@@ -1,7 +1,7 @@
 package it.nicolasfarabegoli.pulverization.core
 
 /**
- * Models the concept of _State_ in the pulverization context.
+ * Models the concept of [State] in the pulverization context.
  * @param S the type of the state.
  */
 interface State<S> {
