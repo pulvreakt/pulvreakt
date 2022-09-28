b6195b70ac1b27f50320077a518208e9e64c5c52
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-28 14:29:04 +0200

    chore(project): setup kotlin multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,112 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
+import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.dokka)
     alias(libs.plugins.ktlint.gradle)
     alias(libs.plugins.detekt.gradle)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.dokka)
 }
+
+val Provider<PluginDependency>.id get() = get().pluginId
 
 allprojects {
+    with(rootProject.libs.plugins) {
+        apply(plugin = kotlin.multiplatform.id)
+        apply(plugin = detekt.gradle.id)
+        apply(plugin = ktlint.gradle.id)
+        apply(plugin = dokka.id)
+    }
+
     repositories {
+        google()
         mavenCentral()
     }
-}
 
-subprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "org.jetbrains.dokka")
+    kotlin {
+        jvm {
+            compilations.all {
+                kotlinOptions.jvmTarget = "1.8"
+            }
+            testRuns["test"].executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
+        js(IR) {
+            browser()
+            nodejs()
+            binaries.library()
+        }
+        val hostOs = System.getProperty("os.name").trim().toLowerCaseAsciiOnly()
+        val hostArch = System.getProperty("os.arch").trim().toLowerCaseAsciiOnly()
+        val nativeTarget: (String, org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget.() -> Unit) -> KotlinTarget =
+            when (hostOs to hostArch) {
+                "linux" to "aarch64" -> ::linuxArm64
+                "linux" to "amd64" -> ::linuxX64
+                "linux" to "arm", "linux" to "arm32" -> ::linuxArm32Hfp
+                "linux" to "mips", "linux" to "mips32" -> ::linuxMips32
+                "linux" to "mipsel", "linux" to "mips32el" -> ::linuxMipsel32
+                "mac os x" to "aarch64" -> ::macosArm64
+                "mac os x" to "amd64", "mac os x" to "x86_64" -> ::macosX64
+                "windows" to "amd64", "windows server 2022" to "amd64" -> ::mingwX64
+                "windows" to "x86" -> ::mingwX86
+                else -> throw GradleException("Host OS '$hostOs' with arch '$hostArch' is not supported in Kotlin/Native.")
+            }
+        nativeTarget("native") {
+            binaries {
+                sharedLib()
+                staticLib()
+                // Remove if it is not executable
+//                "main".let { executable ->
+//                    executable {
+//                        entryPoint = executable
+//                    }
+//                    // Enable wasm32
+//                wasm32 {
+//                    binaries {
+//                        executable {
+//                            entryPoint = executable
+//                        }
+//                    }
+//                }
+//                }
+            }
+        }
 
-    dependencies {
-        testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-        testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+        sourceSets {
+            val commonMain by getting {
+                dependencies {
+                    implementation(rootProject.libs.kotlin.stdlib)
+                }
+            }
+            val commonTest by getting {
+                dependencies {
+                    implementation(rootProject.libs.bundles.kotlin.testing.common)
+                }
+            }
+            val jvmTest by getting {
+                dependencies {
+                    implementation(rootProject.libs.bundles.kotlin.testing.jvm)
+                }
+            }
+            val nativeMain by getting {
+                dependsOn(commonMain)
+            }
+            val nativeTest by getting {
+                dependsOn(commonTest)
+            }
+        }
+        targets.all {
+            compilations.all {
+                kotlinOptions {
+                    allWarningsAsErrors = true
+                }
+            }
+        }
     }
 
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
+    detekt {
+        config = files("detekt.yml")
     }
 }
