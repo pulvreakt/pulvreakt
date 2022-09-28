88f2318a2ca7579cc3e0fc9b45b9aaad002545f2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-28 16:40:28 +0200

    feat(platform): define platform interface

---
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platform/PulverizationPlatform.kt
@@ -0,0 +1,28 @@
+package it.nicolasfarabegoli.pulverization.platform
+
+import it.nicolasfarabegoli.pulverization.core.*
+
+interface PulverizationPlatform<I, S : State<S>, E, SendType, ReceiveType, Outcome> {
+    val repetitions: Int
+    val device: Device<I, S, E, SendType, ReceiveType, Outcome>
+
+    fun contextAcquisition(sensors: SensorsContainer<I>, communication: () -> ReceiveType)
+    fun computation(state: State<S>, behaviour: Behaviour<S, E, SensorPayload, ActuatorPayload, Outcome>)
+    fun dataPropagation(state: State<S>, communication: (payload: SendType) -> Unit)
+    fun actuation(state: State<S>, actuators: ActuatorsContainer<I>)
+
+    private fun cycle() {
+        contextAcquisition(device.sensorsContainer, device.communication::receive)
+        computation(device.state, device.behaviour)
+        dataPropagation(device.state, device.communication::send)
+        actuation(device.state, device.actuatorsContainer)
+    }
+
+    suspend fun start() {
+        repeat(repetitions) { cycle() }
+    }
+
+    suspend fun infiniteStart() {
+        while (true) { cycle() }
+    }
+}
