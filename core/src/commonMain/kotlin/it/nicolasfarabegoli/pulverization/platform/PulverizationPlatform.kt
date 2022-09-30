d5914d512be7a3ec42289fbc6c79d41f7f6f25cf
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-09-30 12:15:22 +0200

    fix: make suspendable some blocking methods

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platform/PulverizationPlatform.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platform/PulverizationPlatform.kt
@@ -5,17 +5,17 @@ import it.nicolasfarabegoli.pulverization.core.*
 /**
  * Represent the platform in which a [Device] runs.
  */
-interface PulverizationPlatform<I, S : State<S>, E, SendType, ReceiveType, Outcome> {
+interface PulverizationPlatform<I, S, E, SendType, ReceiveType, Outcome> {
     val repetitions: Int
     val device: Device<I, S, E, SendType, ReceiveType, Outcome>
 
-    fun contextAcquisition(sensors: SensorsContainer<I>, communication: () -> ReceiveType)
+    suspend fun contextAcquisition(state: State<S>, sensors: SensorsContainer<I>, communication: suspend () -> ReceiveType)
     fun computation(state: State<S>, behaviour: Behaviour<S, E, SensorPayload, ActuatorPayload, Outcome>)
-    fun dataPropagation(state: State<S>, communication: (payload: SendType) -> Unit)
+    suspend fun dataPropagation(state: State<S>, communication: suspend (payload: SendType) -> Unit)
     fun actuation(state: State<S>, actuators: ActuatorsContainer<I>)
 
-    private fun cycle() {
-        contextAcquisition(device.sensorsContainer, device.communication::receive)
+    private suspend fun cycle() {
+        contextAcquisition(device.state, device.sensorsContainer, device.communication::receive)
         computation(device.state, device.behaviour)
         dataPropagation(device.state, device.communication::send)
         actuation(device.state, device.actuatorsContainer)
