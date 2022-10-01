2e4a9060fbdf1473c6e25921c0b9570dbf23949b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-01 16:23:36 +0200

    refactor: refactor containers

--- core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/ActuatorsFixtures.kt
+++ core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/ActuatorsFixtures.kt
@@ -1,17 +1,17 @@
 package it.nicolasfarabegoli.pulverization.core
 
 object ActuatorsFixtures {
-    class MyActuator1(override val id: Int) : Actuator<ActuatorPayload, Int> {
-        override fun actuate(payload: ActuatorPayload) = TODO("Not yet implemented")
+    class MyActuator1(override val id: Int) : Actuator<Int, Int> {
+        override fun actuate(payload: Int) = TODO("Not yet implemented")
     }
 
-    data class MyPayload(val p: Double) : ActuatorPayload
+    data class MyPayload(val p: Double)
     class MyActuator2(override val id: Int) : Actuator<MyPayload, Int> {
         override fun actuate(payload: MyPayload) = TODO("Not yet implemented")
     }
 
-    class DummyActuator(override val id: Int) : Actuator<ActuatorPayload, Int> {
-        override fun actuate(payload: ActuatorPayload) {
+    class DummyActuator(override val id: Int) : Actuator<Int, Int> {
+        override fun actuate(payload: Int) {
             TODO("Not yet implemented")
         }
     }
--- core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/SensorsFixtures.kt
+++ core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/SensorsFixtures.kt
@@ -1,19 +1,19 @@
 package it.nicolasfarabegoli.pulverization.core
 
 object SensorsFixtures {
-    class MySensor1(override val id: Int) : Sensor<SensorPayload, Int> {
-        override fun sense(): SensorPayload {
+    class MySensor1(override val id: Int) : Sensor<Int, Int> {
+        override fun sense(): Int {
             TODO("Not yet implemented")
         }
     }
 
-    class MySensor2(override val id: Int) : Sensor<SensorPayload, Int> {
-        override fun sense(): SensorPayload {
+    class MySensor2(override val id: Int) : Sensor<Int, Int> {
+        override fun sense(): Int {
             TODO("Not yet implemented")
         }
     }
 
-    data class MyPayload(val p: Double) : SensorPayload
+    data class MyPayload(val p: Double)
     class DummySensor(override val id: Int) : Sensor<MyPayload, Int> {
         override fun sense(): MyPayload {
             TODO("Not yet implemented")
