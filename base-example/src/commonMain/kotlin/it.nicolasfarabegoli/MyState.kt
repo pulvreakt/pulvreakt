3d54d8622adcaae1d19d763f1e36d2c5b17b1d27
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-03 21:45:45 +0200

    chore(build): remove unused example folder

--- base-example/src/commonMain/kotlin/it.nicolasfarabegoli/MyState.kt
+++
@@ -1,5 +0,0 @@
-package it.nicolasfarabegoli
-
-import it.nicolasfarabegoli.pulverization.core.State
-
-class MyState(override var state: Int) : State<Int>
--- base-example/src/commonMain/kotlin/it.nicolasfarabegoli/NetworkManager.kt
+++
@@ -1,8 +0,0 @@
-package it.nicolasfarabegoli
-
-import it.nicolasfarabegoli.pulverization.core.Communication
-
-expect class NetworkManager : Communication<NetworkSend, NetworkReceive>
-
-data class NetworkSend(val payload: String)
-data class NetworkReceive(val payload: List<String>)
