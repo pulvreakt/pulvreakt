9ca246b6c221376ef162b6815db13f86d2defd48
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-03 12:24:21 +0200

    chore: fix site url

--- docsite/docusaurus.config.js
+++ docsite/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://nicolasfarabegoli.it',
+  baseUrl: '/pulverization-framework/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
