2ca949978daeff28fbdf708bf3c2a531275a65b6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-04 18:02:44 +0200

    style: rename interface

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ComponentCommunicator.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ComponentCommunicator.kt
@@ -3,18 +3,18 @@ package it.nicolasfarabegoli.pulverization.communication
 /**
  * Models the ability to (only) send messages to other components.
  */
-interface SenderCommunicator<in P, in T> {
-    fun sendToComponent(payload: P, to: T)
+interface SenderCommunicator<in P, in I> {
+    fun sendToComponent(payload: P, to: I)
 }
 
 /**
  * Models the ability to (only) receive messages from other components.
  */
-interface ReceiverCommunicator<out P, in T> {
-    fun receiveFromComponent(from: T): P
+interface ReceiverCommunicator<out P, in I> {
+    fun receiveFromComponent(from: I): P
 }
 
 /**
  * Models the ability to send and receive messages to and from other components.
  */
-interface ComponentCommunicatorBidirectional<in PS, out PR, T> : SenderCommunicator<PS, T>, ReceiverCommunicator<PR, T>
+interface BidirectionalCommunicator<in PS, out PR, I> : SenderCommunicator<PS, I>, ReceiverCommunicator<PR, I>
