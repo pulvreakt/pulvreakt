f5671e03cd4aadd5b588f468576ea42ccce43f42
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-05 09:47:42 +0200

    refactor: change filed visibility to protected

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/SensorsCommunicator.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/SensorsCommunicator.kt
@@ -1,9 +1,17 @@
 package it.nicolasfarabegoli.pulverization.communication
 
+import it.nicolasfarabegoli.pulverization.core.Sensor
 import it.nicolasfarabegoli.pulverization.core.SensorsContainer
+import it.nicolasfarabegoli.pulverization.core.State
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
+/**
+ * Enable the communication from the [Sensor]s to the state.
+ * How to reach or communicate with the [State] component is up to the concrete class.
+ * @param I the type of the identifier of the components.
+ * @param P the type of the payload to send to the [State].
+ */
 abstract class SensorsCommunicator<P, I> : KoinComponent, SenderCommunicator<P, I> {
-    private val sensorsContainer: SensorsContainer<I> by inject()
+    protected val sensorsContainer: SensorsContainer<I> by inject()
 }
