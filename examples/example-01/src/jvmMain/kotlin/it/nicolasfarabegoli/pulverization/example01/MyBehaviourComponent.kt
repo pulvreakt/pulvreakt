2b9c8ddb70373f791a3f5f9c017be4b770469c14
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-05 18:39:15 +0200

    chore: add rabbitmq scheleton

--- examples/example-01/src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/example01/MyBehaviourComponent.kt
+++ examples/example-01/src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/example01/MyBehaviourComponent.kt
@@ -1,16 +1,41 @@
 package it.nicolasfarabegoli.pulverization.example01
 
+import com.rabbitmq.client.CancelCallback
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
 import it.nicolasfarabegoli.pulverization.component.DeviceComponent
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.nio.charset.StandardCharsets
 
 actual class MyBehaviourComponent(override val deviceID: String) : DeviceComponent<OutgoingMessages, Unit, String>, KoinComponent {
     private val state: MyState by inject()
     private val behaviour: MyBehaviour by inject()
+    private val channel: Channel
+    private val deliverCallback = DeliverCallback { consumerTag, deliver ->
+        val message = String(deliver.body, StandardCharsets.UTF_8)
+        println("[$consumerTag] Received message: $message")
+    }
+    private val cancelCallback = CancelCallback { consumerTag ->
+        println("[$consumerTag] was canceled")
+    }
+
+    init {
+        val connection = ConnectionFactory()
+        connection.newConnection("amqp://guest:guest@localhost:5672/").let { conn ->
+            conn.createChannel().let { channel ->
+                channel.queueDeclare("communication/$deviceID/inbox", false, false, false, null)
+                channel.queueDeclare("communication/$deviceID/outbox", false, false, false, null)
+                channel.basicConsume("communication/$deviceID/outbox", true, "SimpleConsumer", deliverCallback, cancelCallback)
+                this.channel = channel
+            }
+        }
+    }
 
     override fun sendToComponent(payload: OutgoingMessages, to: String) {
         when (payload) {
-            is OutgoingMessages.SendMyState -> println(payload.state)
+            is OutgoingMessages.SendMyState -> channel.basicPublish("", "communication/$deviceID/inbox", null, payload.state.toByteArray())
         }
     }
 
--- examples/example-01/src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/example01/MyCommunication.kt
+++ examples/example-01/src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/example01/MyCommunication.kt
@@ -1,9 +1,15 @@
 package it.nicolasfarabegoli.pulverization.example01
 
+import com.rabbitmq.client.AMQP.Basic.Deliver
+import com.rabbitmq.client.CancelCallback
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
 import it.nicolasfarabegoli.pulverization.component.DeviceComponent
 import it.nicolasfarabegoli.pulverization.core.Communication
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.nio.charset.StandardCharsets
 
 actual class MyCommunication : Communication<String, String> {
     override fun send(payload: String) {
@@ -17,6 +23,27 @@ actual class MyCommunication : Communication<String, String> {
 
 actual class MyCommunicationComponent(override val deviceID: String) : DeviceComponent<String, String, String>, KoinComponent {
     private val myCommunication: MyCommunication by inject()
+    private val channel: Channel
+    private val deliverCallback = DeliverCallback { consumerTag, deliver ->
+        val message = String(deliver.body, StandardCharsets.UTF_8)
+        println("[$consumerTag] Received message: $message")
+    }
+    private val cancelCallback = CancelCallback { consumerTag ->
+        println("[$consumerTag] was canceled")
+    }
+
+    init {
+        val connection = ConnectionFactory()
+        connection.newConnection("amqp://guest:guest@localhost:5672/").let { conn ->
+            conn.createChannel().let { channel ->
+                channel.queueDeclare("communication/$deviceID/inbox", false, false, false, null)
+                channel.queueDeclare("communication/$deviceID/outbox", false, false, false, null)
+                // TODO: declare neighbours queues
+                channel.basicConsume("communication/$deviceID/inbox", true, "SimpleConsumer", deliverCallback, cancelCallback)
+                this.channel = channel
+            }
+        }
+    }
 
     override fun sendToComponent(payload: String, to: String) {
         println("Send $payload to Behaviour component")
