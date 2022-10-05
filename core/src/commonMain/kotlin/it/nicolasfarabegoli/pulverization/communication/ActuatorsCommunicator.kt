7599e4d09bf9748f69d787a28e0a3f307de652ce
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-05 11:54:21 +0200

    docs: improve kdoc

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ActuatorsCommunicator.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ActuatorsCommunicator.kt
@@ -2,15 +2,15 @@ package it.nicolasfarabegoli.pulverization.communication
 
 import it.nicolasfarabegoli.pulverization.core.Actuator
 import it.nicolasfarabegoli.pulverization.core.ActuatorsContainer
-import it.nicolasfarabegoli.pulverization.core.State
+import it.nicolasfarabegoli.pulverization.core.Behaviour
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 /**
  * Enable the communication from the [Actuator]s to the state.
- * How to reach or communicate with the [State] component is up to the concrete class.
+ * How to reach (or communicate with) the [Behaviour] component is up to the concrete class.
  * @param I the type of the identifier of the components.
- * @param R the type of the payload to receive from the [State].
+ * @param R the type of the payload to receive from the [Behaviour].
  */
 abstract class ActuatorsCommunicator<R, I> : KoinComponent, ReceiverCommunicator<R, I> {
     protected val actuatorsContainer: ActuatorsContainer<I> by inject()
