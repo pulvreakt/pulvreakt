08c4bdf33156511898c2c173a6f9f61dfbaf747a
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-05 11:54:21 +0200

    docs: improve kdoc

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/SensorsCommunicator.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/SensorsCommunicator.kt
@@ -1,16 +1,16 @@
 package it.nicolasfarabegoli.pulverization.communication
 
+import it.nicolasfarabegoli.pulverization.core.Behaviour
 import it.nicolasfarabegoli.pulverization.core.Sensor
 import it.nicolasfarabegoli.pulverization.core.SensorsContainer
-import it.nicolasfarabegoli.pulverization.core.State
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 /**
  * Enable the communication from the [Sensor]s to the state.
- * How to reach or communicate with the [State] component is up to the concrete class.
+ * How to reach (or communicate with) the [Behaviour] component is up to the concrete class.
  * @param I the type of the identifier of the components.
- * @param P the type of the payload to send to the [State].
+ * @param P the type of the payload to send to the [Behaviour].
  */
 abstract class SensorsCommunicator<P, I> : KoinComponent, SenderCommunicator<P, I> {
     protected val sensorsContainer: SensorsContainer<I> by inject()
