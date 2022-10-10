796909c3268fabb17040e962c3301e18b404f4e0
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-10 09:36:26 +0200

    refactor: refactored the DeviceComponent interface

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/component/DeviceComponent.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/component/DeviceComponent.kt
@@ -1,39 +1,31 @@
 package it.nicolasfarabegoli.pulverization.component
 
 import it.nicolasfarabegoli.pulverization.communication.BidirectionalCommunicator
+import it.nicolasfarabegoli.pulverization.communication.ReceiverCommunicator
+import it.nicolasfarabegoli.pulverization.communication.SenderCommunicator
 
 /**
- * A device component with the ability of a [BidirectionalCommunicator] to communicate with other
- * [DeviceComponent].
+ * Models the concept of _Component_ belonging to a _Device_.
+ * Contains a [deviceID] and a [cycle] method to execute the logic of the component.
  */
-interface DeviceComponent<Send, Receive, I> : BidirectionalCommunicator<Send, Receive, I> {
+interface DeviceComponent<I> {
     val deviceID: I
     suspend fun cycle()
 }
 
-// /**
-// * TODO.
-// */
-// interface DeviceComponent {
-//    suspend fun cycle()
-// }
-//
-// abstract class StateComponent<S, Send, Receive, I> : KoinComponent, DeviceComponent {
-//    protected val state: State<S> by inject()
-//    protected abstract val behaviourCommunicator: BidirectionalCommunicator<Send, Receive, I>
-// }
-//
-// abstract class SensorsComponent<Send, I> : KoinComponent, DeviceComponent {
-//    protected val componentsContainer: SensorsContainer<I> by inject()
-//    protected abstract val behaviourCommunicator: SenderCommunicator<Send, I>
-// }
-//
-// abstract class ActuatorsComponent<Receive, I> : KoinComponent, DeviceComponent {
-//    protected val actuatorsContainer: ActuatorsContainer<I> by inject()
-//    protected abstract val behaviourCommunicator: ReceiverCommunicator<Receive, I>
-// }
-//
-// abstract class CommunicateComponent<PS, PR, Send, Receive, I> : KoinComponent, DeviceComponent {
-//    protected val neighboursCommunication: Communication<PS, PR> by inject()
-//    protected abstract val behaviourCommunicator: BidirectionalCommunicator<Send, Receive, I>
-// }
+/**
+ * Models a [DeviceComponent] with the capability of receiving only messages from other components.
+ * This type of component is _passive_.
+ */
+interface ReceiveOnlyDeviceComponent<out Receive, I> : ReceiverCommunicator<Receive, I>, DeviceComponent<I>
+
+/**
+ * Models a [DeviceComponent] with the capability of only sending messages to other components.
+ * This type of component is _proactive_.
+ */
+interface SendOnlyDeviceComponent<in Send, I> : SenderCommunicator<Send, I>, DeviceComponent<I>
+
+/**
+ * Models a [DeviceComponent] with the capability of both sending and receiving messages from other components.
+ */
+interface SendReceiveDeviceComponent<in Send, out Receive, I> : BidirectionalCommunicator<Send, Receive, I>, DeviceComponent<I>
