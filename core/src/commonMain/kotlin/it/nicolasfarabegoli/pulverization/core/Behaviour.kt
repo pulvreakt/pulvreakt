7d128cac0b4a7b6cd713f7a8fd6464dc29a316fc
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-10 15:00:29 +0200

    refactor: change the representation of sensed values and actuations prescriptions

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/core/Behaviour.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/core/Behaviour.kt
@@ -2,16 +2,16 @@ package it.nicolasfarabegoli.pulverization.core
 
 /**
  * Represents the output of the function modelling the behaviour.
- * Contains the [newState], the [newExport], the [actuation] and the function [outcome].
+ * Contains the [newState], the [newExport], the [actuations] and the function [outcome].
  * @param S the type of the state
  * @param E the type of the export
- * @param A the type of the actuation
+ * @param A the type of the actuations
  * @param O the type of the outcome of the function
  */
 data class BehaviourOutput<S, E, A, O>(
     val newState: S,
     val newExport: E,
-    val actuation: Set<A>,
+    val actuations: A,
     val outcome: O,
 )
 
@@ -20,9 +20,9 @@ data class BehaviourOutput<S, E, A, O>(
  * A [Behaviour] is a pure function of the kind `B(x, e, o) = (x', e', a)`
  * @param S the type of the state
  * @param E the type of the export
- * @param W the type of the sensed value
+ * @param W the type of the sensed values
  * @param A the type of the actuation to do
  */
 interface Behaviour<S, E, W, A, O> {
-    operator fun invoke(state: S, export: E, sensedValues: Set<W>): BehaviourOutput<S, E, A, O>
+    operator fun invoke(state: S, export: E, sensedValues: W): BehaviourOutput<S, E, A, O>
 }
