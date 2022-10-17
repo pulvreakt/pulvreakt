965ba2387eba59925e7d1f061a2f0a44463ce4f0
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-17 11:38:19 +0200

    feat: implement rabbitmq configuration dsl

---
+++ platforms/rabbitmq/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/config/RabbitmqPulverizationConfig.kt
@@ -0,0 +1,40 @@
+package it.nicolasfarabegoli.pulverization.platforms.rabbitmq.config
+
+import it.nicolasfarabegoli.pulverization.config.PulverizationScope
+import it.nicolasfarabegoli.pulverization.config.Topology
+
+/**
+ * Represents the rabbitmq broker configuration.
+ * The configuration holds the [hostname] to connect to and the relative [port].
+ */
+data class BrokerConfiguration(var hostname: String = "rabbitmq", var port: Int = 5672)
+
+/**
+ * represent the final configuration holding the [topology] and the [brokerConfiguration].
+ */
+data class RabbitmqPulverizationConfig<I>(val topology: Topology<I>, val brokerConfiguration: BrokerConfiguration)
+
+/**
+ * Scope for configure rabbitmq.
+ */
+class RabbitmqScope<I> : PulverizationScope<I>() {
+    /**
+     * The broker configuration.
+     */
+    var brokerConfig = BrokerConfiguration()
+
+    /**
+     * Entrypoint for configure the rabbitmq parameters.
+     */
+    fun rabbitmq(init: BrokerConfiguration.() -> Unit) {
+        brokerConfig.apply(init)
+    }
+}
+
+/**
+ * Function for configure the pulverization platform using rabbitmq as lowe infrastructure.
+ */
+fun <I> pulverizationConfiguration(init: RabbitmqScope<I>.() -> Unit): RabbitmqPulverizationConfig<I> {
+    val config = RabbitmqScope<I>().apply(init)
+    return RabbitmqPulverizationConfig(config.topology, config.brokerConfig)
+}
