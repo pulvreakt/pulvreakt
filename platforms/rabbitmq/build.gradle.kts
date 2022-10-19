62bf82dcf161226f7bd1eea76a87aa3e6453e7fc
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-19 16:02:13 +0200

    chore(deps): add rabbitmq and other libraries

--- platforms/rabbitmq/build.gradle.kts
+++ platforms/rabbitmq/build.gradle.kts
@@ -9,7 +9,17 @@ kotlin {
             dependencies {
                 implementation(rootProject.libs.kotlinx.coroutines.reactive)
                 implementation(rootProject.libs.kotlinx.coroutines.reactor)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
                 implementation("io.projectreactor.rabbitmq:reactor-rabbitmq:1.5.5")
+            }
+        }
+        jvmTest {
+            dependencies {
+                implementation(rootProject.libs.kotlinx.coroutines.reactive)
+                implementation(rootProject.libs.kotlinx.coroutines.reactor)
+                implementation(rootProject.libs.kotest.extensions.koin)
+                implementation("com.github.fridujo:rabbitmq-mock:1.1.1")
             }
         }
     }
