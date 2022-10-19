a82118a609f423c9803541f8dd1aa50a4fdcc694
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-19 16:05:11 +0200

    chore: add stubs

---
+++ platforms/rabbitmq/src/jsMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/SimpleRabbitmqSenderCommunicator.kt
@@ -0,0 +1,46 @@
+package it.nicolasfarabegoli.pulverization.platforms.rabbitmq.communication
+
+import it.nicolasfarabegoli.pulverization.core.DeviceID
+import kotlinx.coroutines.flow.Flow
+import org.koin.core.component.KoinComponent
+
+/**
+ * Simple implementation for communicate (send only) with another component using RabbitMQ.
+ */
+actual class SimpleRabbitmqSenderCommunicator<in Send, I : DeviceID>(
+    override val id: I,
+    override val queue: String,
+) : RabbitmqSenderCommunicator<Send, I>, KoinComponent {
+    override suspend fun sendToComponent(payload: Send, to: I?) {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * Simple implementation for communicate (receive only) with another component using RabbitMQ.
+ */
+actual class SimpleRabbitmqReceiverCommunicator<out Receive, I : DeviceID>(
+    override val id: I,
+    override val queue: String,
+) : RabbitmqReceiverCommunicator<Receive, I>, KoinComponent {
+    override fun receiveFromComponent(): Flow<Receive> {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * Simple implementation for communicate with another component using RabbitMQ.
+ */
+actual class SimpleRabbitmqBidirectionalCommunication<in Send, out Receive, I : DeviceID>(
+    override val id: I,
+    override val queue: String,
+) :
+    RabbitmqBidirectionalCommunicator<Send, Receive, I> {
+    override suspend fun sendToComponent(payload: Send, to: I?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun receiveFromComponent(): Flow<Receive> {
+        TODO("Not yet implemented")
+    }
+}
