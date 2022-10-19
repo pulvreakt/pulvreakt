00e4bbc3878735f54abf1a090b7b207060e6f042
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-19 10:32:36 +0200

    refactor: add id to all the interfaces

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ComponentCommunicator.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/communication/ComponentCommunicator.kt
@@ -7,21 +7,31 @@ import kotlinx.coroutines.flow.emptyFlow
 /**
  * Models the ability to (only) send messages to other components.
  */
-interface SenderCommunicator<in Send, in I : DeviceID> {
+interface SenderCommunicator<in Send, I : DeviceID> {
+    /**
+     * The device ID.
+     */
+    val id: I
+
     /**
      * Send the [payload] [to] another component.
      */
-    fun sendToComponent(payload: Send, to: I? = null)
+    suspend fun sendToComponent(payload: Send, to: I? = null)
 }
 
 /**
  * Models the ability to (only) receive messages from other components.
  */
-interface ReceiverCommunicator<out Receive, in I : DeviceID> {
+interface ReceiverCommunicator<out Receive, I : DeviceID> {
+    /**
+     * The device ID.
+     */
+    val id: I
+
     /**
      * Receive a message [from] a component.
      */
-    fun receiveFromComponent(from: I? = null): Receive?
+    suspend fun receiveFromComponent(from: I? = null): Receive?
 
     /**
      * Creates an async flow with all received messages.
