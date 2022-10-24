89b8972077e4b90b036f45049cb650055972e7df
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-24 11:57:44 +0200

    feat: add initialize method to all rabbitmq communicators

--- platforms/rabbitmq/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/RabbitmqCommunicator.kt
+++ platforms/rabbitmq/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/RabbitmqCommunicator.kt
@@ -6,35 +6,36 @@ import it.nicolasfarabegoli.pulverization.communication.SenderCommunicator
 import it.nicolasfarabegoli.pulverization.core.DeviceID
 
 /**
- * Represents a sender-only communicator backed by RabbitMQ as communication technology.
- * The communication is mediated by a [queue].
+ * Add rabbitmq specific stuff.
  */
-interface RabbitmqSenderCommunicator<in Send, I : DeviceID> : SenderCommunicator<Send, I> {
+interface RabbitmqCommunicator {
     /**
      * The name of the queue in which the communications flows.
      */
     val queue: String
+
+    /**
+     * Initialize the communicator component.
+     */
+    suspend fun initialize() {}
 }
+
+/**
+ * Represents a sender-only communicator backed by RabbitMQ as communication technology.
+ * The communication is mediated by a [queue].
+ */
+interface RabbitmqSenderCommunicator<Send : Any, I : DeviceID> : SenderCommunicator<Send, I>, RabbitmqCommunicator
 
 /**
  * Represents a receiver-only communicator backed by RabbitMQ as a communication technology.
  * The communication is mediated by a [queue].
  */
-interface RabbitmqReceiverCommunicator<out Receive, I : DeviceID> : ReceiverCommunicator<Receive, I> {
-    /**
-     * The name of the queue in which the communications flows.
-     */
-    val queue: String
-}
+interface RabbitmqReceiverCommunicator<Receive : Any, I : DeviceID> :
+    ReceiverCommunicator<Receive, I>, RabbitmqCommunicator
 
 /**
  * Represents a bidirectional communication backed by RabbitMQ as a communication technology.
  * The communication is mediated by a [queue].
  */
 interface RabbitmqBidirectionalCommunicator<in Send, out Receive, I : DeviceID> :
-    BidirectionalCommunicator<Send, Receive, I> {
-    /**
-     * The name of the queue in which the communications flows.
-     */
-    val queue: String
-}
+    BidirectionalCommunicator<Send, Receive, I>, RabbitmqCommunicator
