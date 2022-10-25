22cc0b81a5a6d41dd810a6fb93774c0babc3b4f6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-25 09:52:17 +0200

    chore: try new solution with a generic Device component

--- platforms/rabbitmq/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/SimpleRabbitmqCommunicator.kt
+++ platforms/rabbitmq/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/SimpleRabbitmqCommunicator.kt
@@ -2,24 +2,59 @@ package it.nicolasfarabegoli.pulverization.platforms.rabbitmq.communication
 
 import it.nicolasfarabegoli.pulverization.core.DeviceID
 import org.koin.core.component.KoinComponent
+import kotlin.reflect.KClass
 
 /**
  * Simple implementation for communicate (send only) with another component using RabbitMQ.
  */
-expect class SimpleRabbitmqSenderCommunicator<Send : Any, I : DeviceID> :
-    RabbitmqSenderCommunicator<Send, I>,
-    KoinComponent
+expect class SimpleRabbitmqSenderCommunicator<Send : Any, I : DeviceID>(
+    type: KClass<Send>,
+    id: I,
+    queue: String,
+) : RabbitmqSenderCommunicator<Send, I>,
+    KoinComponent {
+
+    companion object {
+        inline operator fun <reified S : Any> invoke(
+            id: DeviceID,
+            queue: String,
+        ): SimpleRabbitmqSenderCommunicator<S, DeviceID>
+    }
+}
 
 /**
  * Simple implementation for communicate (receive only) with another component using RabbitMQ.
  */
-expect class SimpleRabbitmqReceiverCommunicator<Receive : Any, I : DeviceID> :
-    RabbitmqReceiverCommunicator<Receive, I>,
-    KoinComponent
+expect class SimpleRabbitmqReceiverCommunicator<Receive : Any, I : DeviceID>(
+    type: KClass<Receive>,
+    id: I,
+    queue: String,
+) : RabbitmqReceiverCommunicator<Receive, I>,
+    KoinComponent {
+
+    companion object {
+        inline operator fun <reified R : Any> invoke(
+            id: DeviceID,
+            queue: String,
+        ): SimpleRabbitmqReceiverCommunicator<R, DeviceID>
+    }
+}
 
 /**
  * Simple implementation for communicate with another component using RabbitMQ.
  */
-expect class SimpleRabbitmqBidirectionalCommunication<Send : Any, Receive : Any, I : DeviceID> :
-    RabbitmqBidirectionalCommunicator<Send, Receive, I>,
-    KoinComponent
+expect class SimpleRabbitmqBidirectionalCommunication<Send : Any, Receive : Any, I : DeviceID>(
+    kSend: KClass<Send>,
+    kReceive: KClass<Receive>,
+    id: I,
+    queue: String,
+) : RabbitmqBidirectionalCommunicator<Send, Receive, I>,
+    KoinComponent {
+
+    companion object {
+        inline operator fun <reified S : Any, reified R : Any> invoke(
+            id: DeviceID,
+            queue: String,
+        ): SimpleRabbitmqBidirectionalCommunication<S, R, DeviceID>
+    }
+}
