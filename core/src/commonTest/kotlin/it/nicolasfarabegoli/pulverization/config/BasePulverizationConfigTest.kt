dbafe828b6b7717737572f036e13d2f46f0c364d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-29 12:38:57 +0200

    test: setup test for new DSL

--- core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/config/BasePulverizationConfigTest.kt
+++ core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/config/BasePulverizationConfigTest.kt
@@ -1,45 +1,42 @@
 package it.nicolasfarabegoli.pulverization.config
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldContainExactly
-import it.nicolasfarabegoli.pulverization.core.DeviceID
-import it.nicolasfarabegoli.pulverization.core.DeviceIDOps.toID
-import it.nicolasfarabegoli.pulverization.core.LogicalDevice
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
+import it.nicolasfarabegoli.pulverization.core.get
 
 class BasePulverizationConfigTest : FunSpec(
     {
         context("Test the configuration DSL") {
-            val d1 = LogicalDevice("1".toID())
-            val d2 = LogicalDevice("2".toID())
-            val d3 = LogicalDevice("3".toID())
-            test("The DSL should give the possibility to define a custom topology") {
-                val configuration = pulverizationConfiguration {
-                    topology {
-                        createLinks(d1, setOf(d2, d3))
+            test("TODO: name test") {
+                val config = pulverizationConfig {
+                    logicalDevice("device-1") {
+                        component(DSLFixtures.MyState())
                     }
                 }
-                testNeighbours(configuration, d1, setOf(d2, d3))
-                testNeighbours(configuration, d2, setOf(d1))
-                testNeighbours(configuration, d3, setOf(d1))
+                (config.devices["device-1"]?.get<DSLFixtures.MyState>() ?: error("")) shouldNotBe null
+                (config.devices["device-1"]?.get<DSLFixtures.MyState2>() shouldBe null) shouldNotBe null
             }
-            test("The DSL should give the possibility to define a fully-connected network") {
-                val configuration = pulverizationConfiguration {
-                    fullyConnectedTopology(d1, d2, d3)
+            test("An exception should occur when try to configure a logical device with the same name") {
+                val exc = shouldThrow<Exception> {
+                    pulverizationConfig {
+                        logicalDevice("device-1") { }
+                        logicalDevice("device-1") { }
+                    }
                 }
-                testNeighbours(configuration, d1, setOf(d2, d3))
-                testNeighbours(configuration, d2, setOf(d1, d3))
-                testNeighbours(configuration, d3, setOf(d1, d2))
+                exc.message shouldContain "device-1"
+            }
+            test("No exception should be thrown on a right configuration") {
+                shouldNotThrow<IllegalStateException> {
+                    pulverizationConfig {
+                        logicalDevice("device-1") { }
+                        logicalDevice("device-2") { }
+                    }
+                }
             }
         }
     },
 )
-
-internal fun <I : DeviceID> testNeighbours(
-    config: PulverizationConfig<I>,
-    device: LogicalDevice<I>,
-    expectedNeighbours: Set<LogicalDevice<I>>,
-) {
-    config.topology[device]?.let {
-        it shouldContainExactly expectedNeighbours
-    } ?: error("The device $device should be in the configuration")
-}
