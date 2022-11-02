7ab7d8734c46a1e74339991d51743d8191cca8b9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-02 15:46:33 +0100

    test: test rabbitmq platform

--- platforms/rabbitmq/src/jvmTest/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/SimpleRabbitmqSenderCommunicatorTest.kt
+++ platforms/rabbitmq/src/jvmTest/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/communication/SimpleRabbitmqSenderCommunicatorTest.kt
@@ -1,21 +1,38 @@
 package it.nicolasfarabegoli.pulverization.platforms.rabbitmq.communication
 
-import com.github.fridujo.rabbitmq.mock.MockConnectionFactory
 import com.rabbitmq.client.BuiltinExchangeType
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
+import com.rabbitmq.client.ConnectionFactory
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.koin.KoinExtension
+import io.kotest.matchers.shouldBe
+import it.nicolasfarabegoli.pulverization.core.BehaviourComponent
 import it.nicolasfarabegoli.pulverization.core.DeviceIDOps.toID
+import it.nicolasfarabegoli.pulverization.core.SensorsComponent
 import it.nicolasfarabegoli.pulverization.platforms.rabbitmq.component.RabbitmqContext
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.withTimeout
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.get
+import reactor.rabbitmq.RabbitFlux
+import reactor.rabbitmq.ReceiverOptions
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 class SimpleRabbitmqSenderCommunicatorTest : KoinTest, FunSpec() {
     private val koinModule = module {
-        single<Connection> { MockConnectionFactory().newConnection() }
+        single<Connection> {
+            val cf = ConnectionFactory()
+            cf.useNio()
+            cf.newConnection()
+        }
         single { RabbitmqContext("1".toID()) }
     }
 
@@ -32,23 +49,27 @@ class SimpleRabbitmqSenderCommunicatorTest : KoinTest, FunSpec() {
 
     init {
         context("Sender only component that use RabbitMQ") {
-//            test("The sender only component should create the queue correctly") {
-//                val queue = "test_queue"
-//                val exchange = "pulverization.exchange"
-//                val routingKey = "1"
-//                initQueue(get(), queue, exchange, routingKey)
-//                val options = ReceiverOptions().connectionSupplier { get() }
-//                val receiver = RabbitFlux.createReceiver(options)
-//                val result = async {
-//                    withTimeout(2.toDuration(DurationUnit.SECONDS)) {
-//                        receiver.consumeAutoAck(queue).asFlow().take(1).collect { payload ->
-//                            payload.body shouldNotBe null
-//                        }
-//                    }
-//                }
-//                SimpleRabbitmqSenderCommunicator<String>(exchange, queue).sendToComponent("")
-//                result.await()
-//            }
+            test("The sender only component should create the queue correctly") {
+                val options = ReceiverOptions().connectionSupplier { get() }
+                val receiver = RabbitFlux.createReceiver(options)
+
+                val sender = SimpleRabbitmqSenderCommunicator<String>(SensorsComponent to BehaviourComponent)
+                sender.initialize()
+
+                val result = async {
+                    withTimeout(2.toDuration(DurationUnit.SECONDS)) {
+                        receiver.consumeAutoAck("sensors/behaviour1").asFlow().take(1).collect { payload ->
+                            Json.decodeFromString<String>(payload.body.decodeToString()) shouldBe "hello"
+                        }
+                    }
+                }
+                sender.sendToComponent("hello")
+                result.await()
+
+                // Release resources
+                receiver.close()
+                sender.finalize()
+            }
         }
     }
 }
