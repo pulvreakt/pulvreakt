697c23e776ee376b6a6919a86a16bbf01461518d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-11 17:02:17 +0100

    refactor: refactoring of the configuration DSL

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/dsl/BasePulverizationConfig.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/dsl/BasePulverizationConfig.kt
@@ -1,130 +1,14 @@
-package it.nicolasfarabegoli.pulverization.config
+package it.nicolasfarabegoli.pulverization.dsl
 
 import it.nicolasfarabegoli.pulverization.core.LogicalDevice
 import it.nicolasfarabegoli.pulverization.core.PulverizedComponent
+import kotlin.reflect.KClass
 
 /**
  * Annotation class used for scoping correctly the DSL.
  */
 @DslMarker
 annotation class PulverizationMarker
-
-/**
- * TODO.
- */
-data class LogicalDeviceConfiguration(val components: Map<List<PulverizedComponent>, Tier>)
-
-/**
- * Base configuration for the pulverized system.
- */
-interface BasePulverizationConfig<D> {
-    /**
-     * All the devices in the configuration.
-     */
-    val devices: Map<D, LogicalDeviceConfiguration>
-}
-
-/**
- * TODO.
- */
-data class BasePulverizationConfigImpl<D>(override val devices: Map<D, LogicalDeviceConfiguration>) :
-    BasePulverizationConfig<D>
-
-inline fun <D, reified C1 : PulverizedComponent> BasePulverizationConfig<D>.changeTheName(name: D, run: (C1) -> Unit) {
-    devices[name]?.let {
-        val componentsList = it.components.keys
-            .filter { e -> e.size == 1 }
-            .firstOrNull { l -> l.any { i -> i is C1 } } ?: error("No component ${C1::class} fount in $name")
-        val component1 = componentsList.filterIsInstance<C1>().firstOrNull()!!
-        run(component1)
-    } ?: error("No device of type $name is found!")
-}
-
-inline fun <D, reified C1, reified C2> BasePulverizationConfig<D>.changeName(name: D, run: (C1, C2) -> Unit)
-    where C1 : PulverizedComponent, C2 : PulverizedComponent {
-    devices[name]?.let {
-        val componentsList = it.components.keys
-            .filter { e -> e.size == 2 }
-            .filter { l -> l.any { i -> i is C1 } }
-            .firstOrNull { l -> l.any { i -> i is C2 } }
-            ?: error("No component ${C1::class.simpleName}, ${C2::class.simpleName} found in $name")
-        val component1 = componentsList.filterIsInstance<C1>().firstOrNull()!!
-        val component2 = componentsList.filterIsInstance<C2>().firstOrNull()!!
-        run(component1, component2)
-    } ?: error("No device of type $name is found!")
-}
-
-inline fun <D, reified C1, reified C2, reified C3> BasePulverizationConfig<D>.changeName(
-    name: D,
-    run: (C1, C2, C3) -> Unit,
-) where C1 : PulverizedComponent, C2 : PulverizedComponent, C3 : PulverizedComponent {
-    devices[name]?.let {
-        val componentsList = it.components.keys
-            .filter { e -> e.size == 3 }
-            .filter { l -> l.any { i -> i is C1 } }
-            .filter { l -> l.any { i -> i is C2 } }
-            .firstOrNull { l -> l.any { i -> i is C3 } }
-            ?: error(
-                "No component ${C1::class.simpleName}, ${C2::class.simpleName}, ${C3::class.simpleName} found in $name",
-            )
-        val component1 = componentsList.filterIsInstance<C1>().firstOrNull()!!
-        val component2 = componentsList.filterIsInstance<C2>().firstOrNull()!!
-        val component3 = componentsList.filterIsInstance<C3>().firstOrNull()!!
-        run(component1, component2, component3)
-    } ?: error("No device of type $name is found!")
-}
-
-inline fun <D, reified C1, reified C2, reified C3, reified C4> BasePulverizationConfig<D>.changeName(
-    name: D,
-    run: (C1, C2, C3, C4) -> Unit,
-) where C1 : PulverizedComponent, C2 : PulverizedComponent, C3 : PulverizedComponent, C4 : PulverizedComponent {
-    devices[name]?.let {
-        val componentsList = it.components.keys
-            .filter { e -> e.size == 4 }
-            .filter { l -> l.any { i -> i is C1 } }
-            .filter { l -> l.any { i -> i is C2 } }
-            .filter { l -> l.any { i -> i is C3 } }
-            .firstOrNull { l -> l.any { i -> i is C4 } }
-            ?: error(
-                "No component ${C1::class.simpleName}, ${C2::class.simpleName}, ${C3::class.simpleName}," +
-                    " ${C4::class.simpleName} found in $name",
-            )
-        val component1 = componentsList.filterIsInstance<C1>().firstOrNull()!!
-        val component2 = componentsList.filterIsInstance<C2>().firstOrNull()!!
-        val component3 = componentsList.filterIsInstance<C3>().firstOrNull()!!
-        val component4 = componentsList.filterIsInstance<C4>().firstOrNull()!!
-        run(component1, component2, component3, component4)
-    } ?: error("No device of type $name is found!")
-}
-
-inline fun <D, reified C1, reified C2, reified C3, reified C4, reified C5> BasePulverizationConfig<D>.changeName(
-    name: D,
-    run: (C1, C2, C3, C4, C5) -> Unit,
-) where C1 : PulverizedComponent,
-        C2 : PulverizedComponent,
-        C3 : PulverizedComponent,
-        C4 : PulverizedComponent,
-        C5 : PulverizedComponent {
-    devices[name]?.let {
-        val componentsList = it.components.keys
-            .filter { e -> e.size == 5 }
-            .filter { l -> l.any { i -> i is C1 } }
-            .filter { l -> l.any { i -> i is C2 } }
-            .filter { l -> l.any { i -> i is C3 } }
-            .filter { l -> l.any { i -> i is C4 } }
-            .firstOrNull { l -> l.any { i -> i is C5 } }
-            ?: error(
-                "No component ${C1::class.simpleName}, ${C2::class.simpleName}, ${C3::class.simpleName}," +
-                    " ${C4::class.simpleName}, ${C5::class.simpleName} found in $name",
-            )
-        val component1 = componentsList.filterIsInstance<C1>().firstOrNull()!!
-        val component2 = componentsList.filterIsInstance<C2>().firstOrNull()!!
-        val component3 = componentsList.filterIsInstance<C3>().firstOrNull()!!
-        val component4 = componentsList.filterIsInstance<C4>().firstOrNull()!!
-        val component5 = componentsList.filterIsInstance<C5>().firstOrNull()!!
-        run(component1, component2, component3, component4, component5)
-    } ?: error("No device of type $name is found!")
-}
 
 /**
  * Scope for building DSL.
@@ -140,79 +24,108 @@ interface Scope<S> {
  * Scope for configuring a specific [LogicalDevice].
  */
 @PulverizationMarker
-class LogicalDeviceScope : Scope<LogicalDeviceConfiguration> {
-    private val componentsMap: MutableMap<List<PulverizedComponent>, Tier> = mutableMapOf()
+class LogicalDeviceScope : Scope<Set<DeploymentUnit>> {
+    private val deploymentUnits: MutableSet<DeploymentUnit> = mutableSetOf()
 
-    infix fun <C : PulverizedComponent> List<C>.deployableOn(tier: Tier) {
-        componentsMap[this] = tier
+    /**
+     * Retrive the component type.
+     */
+    inline fun <reified C : PulverizedComponent> component(): KClass<out C> = C::class
+
+    /**
+     * Define where the deployment unit should be deployed.
+     */
+    infix fun <C : PulverizedComponent> Set<KClass<out C>>.deployableOn(tier: Tier) {
+        deploymentUnits += DeploymentUnit(this, tier)
     }
 
-    infix fun <C : PulverizedComponent> C.deployableOn(tier: Tier) {
-        componentsMap[listOf(this)] = tier
+    /**
+     * Define where the deployment unit should be deployed.
+     */
+    infix fun <C : PulverizedComponent> KClass<out C>.deployableOn(tier: Tier) {
+        deploymentUnits += DeploymentUnit(setOf(this), tier)
     }
 
-    infix fun <C : PulverizedComponent> C.and(other: C): List<C> = listOf(this, other)
-    infix fun <C : PulverizedComponent> List<C>.and(other: C): List<C> = this + other
+    /**
+     * Set the current component in the same deployment unit with [other] component.
+     */
+    infix fun <C : PulverizedComponent> KClass<out C>.and(other: KClass<out C>): Set<KClass<out C>> {
+        if (this == other) error("The same component should not appear in the same deployment unit")
+        return setOf(this, other)
+    }
+
+    /**
+     * Set the current components in the same deployment unit with [other] component.
+     */
+    infix fun <C : PulverizedComponent> Set<KClass<out C>>.and(other: KClass<out C>): Set<KClass<out C>> {
+        if (this.contains(other)) error("The same component should not appear in the same deployment unit")
+        return this + other
+    }
 
     /**
      * Creates a new [LogicalDevice].
      */
-    override fun build(): LogicalDeviceConfiguration = LogicalDeviceConfiguration(componentsMap)
+    override fun build(): Set<DeploymentUnit> {
+        val allComponents = deploymentUnits.flatMap { it.deployableComponents }
+        if (allComponents.distinct().size != allComponents.size) {
+            error("A component appear in more than one deployment unit. Check your configuration!")
+        }
+        return deploymentUnits
+    }
 }
 
-sealed interface Tier
-object Cloud : Tier
-object Edge : Tier
-object Device : Tier
+/**
+ * Scope for creating a link between devices.
+ */
+@PulverizationMarker
+class DevicesLinksScope : Scope<Set<DeviceLink>> {
+    private val devicesConnections: MutableSet<DeviceLink> = mutableSetOf()
+
+    /**
+     * Link a device with another.
+     */
+    infix fun String.linkedWith(that: String) {
+        if (this == that) error("The same device could not be linked with itself")
+        devicesConnections += DeviceLink(this, that)
+    }
+
+    override fun build(): Set<DeviceLink> = devicesConnections
+}
 
 /**
  * Scope for creating [LogicalDevice]s.
  */
 @PulverizationMarker
-open class PulverizationConfigScope<D> : Scope<BasePulverizationConfig<D>> {
-    private val logicalDevices: MutableMap<D, LogicalDeviceConfiguration> = mutableMapOf()
+open class PulverizationConfigScope : Scope<PulverizationConfiguration> {
+    private val logicalDevices: MutableSet<LogicalDeviceConfiguration> = mutableSetOf()
+    private val devicesConnections: MutableSet<DeviceLink> = mutableSetOf()
 
     /**
      * Creates a new [LogicalDevice] assigning to it a [name].
      */
-    fun logicalDevice(name: D, init: LogicalDeviceScope.() -> Unit) {
-        if (logicalDevices.containsKey(name)) error("$name already registered")
-        val lDeviceScope = LogicalDeviceScope().apply(init)
-        logicalDevices[name] = lDeviceScope.build()
+    fun logicalDevice(name: String, init: LogicalDeviceScope.() -> Unit) {
+        if (logicalDevices.map { it.deviceName }.contains(name)) error("$name already registered")
+        val deploymentUnits = LogicalDeviceScope().apply(init).build()
+        val allComponents = deploymentUnits.flatMap { it.deployableComponents }.toSet()
+        logicalDevices += LogicalDeviceConfiguration(name, allComponents, deploymentUnits)
     }
 
     /**
-     * Creates a new [BasePulverizationConfig].
+     * Defines a logical link between devices.
      */
-    override fun build(): BasePulverizationConfig<D> = BasePulverizationConfigImpl(logicalDevices)
+    fun devicesLinks(init: DevicesLinksScope.() -> Unit) {
+        devicesConnections += DevicesLinksScope().apply(init).build()
+    }
+
+    /**
+     * Creates a new [PulverizationConfiguration].
+     */
+    override fun build(): PulverizationConfiguration =
+        PulverizationConfiguration(logicalDevices, DeviceRelationsConfiguration(devicesConnections))
 }
 
 /**
  * Entrypoint for the DSL for building the configuration.
  */
-fun <D> pulverizationConfig(init: PulverizationConfigScope<D>.() -> Unit): BasePulverizationConfig<D> =
-    PulverizationConfigScope<D>().apply(init).build()
-
-// sealed interface Devices
-// object Device1 : Devices
-//
-// data class SS(val i: Int) : StateRepresentation
-// class St : State<SS> {
-//    override val context: Context by inject()
-//    override fun get(): SS {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun update(newState: SS): SS {
-//        TODO("Not yet implemented")
-//    }
-// }
-//
-// fun test() {
-//    val config = pulverizationConfig<Devices> {
-//        logicalDevice(Device1) {
-//            St() and St() deployableOn Cloud
-//            St() deployableOn Device
-//        }
-//    }
-// }
+fun pulverizationConfig(init: PulverizationConfigScope.() -> Unit): PulverizationConfiguration =
+    PulverizationConfigScope().apply(init).build()
