3b1689f1a53eb9d6e7795d23d03356b3bed5cdae
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-11 17:02:17 +0100

    refactor: refactoring of the configuration DSL

---
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/dsl/ConfigurationModel.kt
@@ -0,0 +1,31 @@
+package it.nicolasfarabegoli.pulverization.dsl
+
+import it.nicolasfarabegoli.pulverization.core.PulverizedComponent
+import kotlin.reflect.KClass
+
+sealed interface Tier
+object Cloud : Tier
+object Edge : Tier
+object Device : Tier
+
+data class DeploymentUnit(val deployableComponents: Set<KClass<out PulverizedComponent>>, val where: Tier)
+
+data class LogicalDeviceConfiguration(
+    val deviceName: String,
+    val components: Set<KClass<out PulverizedComponent>>,
+    val deploymentUnits: Set<DeploymentUnit>,
+)
+
+data class DeviceLink(val device: String, val otherDevice: String)
+
+data class DeviceRelationsConfiguration(
+    val links: Set<DeviceLink>,
+)
+
+data class PulverizationConfiguration(
+    val devicesConfig: Set<LogicalDeviceConfiguration>,
+    val devicesLinks: DeviceRelationsConfiguration,
+)
+
+fun PulverizationConfiguration.getDeviceConfiguration(name: String): LogicalDeviceConfiguration? =
+    devicesConfig.firstOrNull { it.deviceName == name }
