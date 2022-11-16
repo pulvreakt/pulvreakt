37b357fb9b63d050fbf445be02e1045d205b7b70
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-16 21:53:15 +0100

    test: refactor fixture tests

--- core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/ActuatorsContainerTest.kt
+++ core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/core/ActuatorsContainerTest.kt
@@ -8,14 +8,14 @@ import io.kotest.matchers.types.shouldBeSameInstanceAs
 import it.nicolasfarabegoli.pulverization.component.Context
 import it.nicolasfarabegoli.pulverization.core.ActuatorsFixtures.MyActuator1
 import it.nicolasfarabegoli.pulverization.core.ActuatorsFixtures.MyActuator2
-import it.nicolasfarabegoli.pulverization.dsl.DSLFixtures
+import it.nicolasfarabegoli.pulverization.dsl.FuzzyContext
 
 class ActuatorsContainerTest : FunSpec(
     {
         context("ActuatorsContainer tests") {
             test("To the container can be added multiple actuators at once") {
                 val container = object : ActuatorsContainer() {
-                    override val context: Context = DSLFixtures.FuzzyContext
+                    override val context: Context = FuzzyContext
                 }
                 container.addAll(MyActuator1(), MyActuator1(), MyActuator2())
                 container.getAll<MyActuator1>().size shouldBe 2
@@ -23,7 +23,7 @@ class ActuatorsContainerTest : FunSpec(
             }
             test("To the container can be added a single actuator") {
                 val container = object : ActuatorsContainer() {
-                    override val context: Context = DSLFixtures.FuzzyContext
+                    override val context: Context = FuzzyContext
                 }
                 container += MyActuator1()
                 container.get<MyActuator1>() shouldNotBe null
@@ -31,7 +31,7 @@ class ActuatorsContainerTest : FunSpec(
             }
             test("The container could be queried using the KClass") {
                 val container = object : ActuatorsContainer() {
-                    override val context: Context = DSLFixtures.FuzzyContext
+                    override val context: Context = FuzzyContext
                 }.apply { addAll(MyActuator1(), MyActuator2()) }
                 container[MyActuator1::class] shouldNotBe null
                 container.getAll(MyActuator2::class).size shouldBe 1
@@ -39,7 +39,7 @@ class ActuatorsContainerTest : FunSpec(
             test("The container, when queried, should return the actuator in the lambda") {
                 val actuator2 = MyActuator2()
                 val container = object : ActuatorsContainer() {
-                    override val context: Context = DSLFixtures.FuzzyContext
+                    override val context: Context = FuzzyContext
                 }.apply {
                     addAll(
                         MyActuator1(),
@@ -52,7 +52,7 @@ class ActuatorsContainerTest : FunSpec(
             }
             test("The container, when queried with an invalid class, should not execute the lambda") {
                 val container = object : ActuatorsContainer() {
-                    override val context: Context = DSLFixtures.FuzzyContext
+                    override val context: Context = FuzzyContext
                 }
                 container.get<MyActuator1> {
                     failure("This lambda should not be executed since no ${MyActuator1::class} is available")
--- core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/dsl/DSLFixtures.kt
+++ core/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/dsl/DSLFixtures.kt
@@ -9,42 +9,40 @@ import it.nicolasfarabegoli.pulverization.core.DeviceIDOps.toID
 import it.nicolasfarabegoli.pulverization.core.State
 import it.nicolasfarabegoli.pulverization.core.StateRepresentation
 
-interface DSLFixtures {
-    data class StateRepr(val i: Int) : StateRepresentation
-    object FuzzyContext : Context {
-        override val id: DeviceID = "1".toID()
+data class StateRepr(val i: Int) : StateRepresentation
+object FuzzyContext : Context {
+    override val id: DeviceID = "1".toID()
+}
+
+data class MyState(override val context: Context = FuzzyContext) : State<StateRepr> {
+    override fun get(): StateRepr {
+        TODO("Not yet implemented")
     }
 
-    data class MyState(override val context: Context = FuzzyContext) : State<StateRepr> {
-        override fun get(): StateRepr {
-            TODO("Not yet implemented")
-        }
+    override fun update(newState: StateRepr): StateRepr {
+        TODO("Not yet implemented")
+    }
+}
 
-        override fun update(newState: StateRepr): StateRepr {
-            TODO("Not yet implemented")
-        }
+class MyState2 : State<StateRepr> {
+    override val context: Context = FuzzyContext
+    override fun get(): StateRepr {
+        TODO("Not yet implemented")
     }
 
-    class MyState2 : State<StateRepr> {
-        override val context: Context = FuzzyContext
-        override fun get(): StateRepr {
-            TODO("Not yet implemented")
-        }
-
-        override fun update(newState: StateRepr): StateRepr {
-            TODO("Not yet implemented")
-        }
+    override fun update(newState: StateRepr): StateRepr {
+        TODO("Not yet implemented")
     }
+}
 
-    data class Payload(val payload: String) : CommunicationPayload
-    class MyBehaviour : Behaviour<StateRepr, Payload, Int, Int, Unit> {
-        override val context: Context = FuzzyContext
-        override fun invoke(
-            state: StateRepr,
-            export: List<Payload>,
-            sensedValues: Int,
-        ): BehaviourOutput<StateRepr, Payload, Int, Unit> {
-            TODO("Not yet implemented")
-        }
+data class Payload(val payload: String) : CommunicationPayload
+class MyBehaviour : Behaviour<StateRepr, Payload, Int, Int, Unit> {
+    override val context: Context = FuzzyContext
+    override fun invoke(
+        state: StateRepr,
+        export: List<Payload>,
+        sensedValues: Int,
+    ): BehaviourOutput<StateRepr, Payload, Int, Unit> {
+        TODO("Not yet implemented")
     }
 }
