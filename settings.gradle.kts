599c82e59a16e8f0bc5485987cc539e917913df6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-18 16:37:15 +0100

    chore(build): add runtime subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "pulverization-framework"
 
 include(":core")
+include(":runtime")
 // include(":examples:example-01")
 include(":rabbitmq-platform")
 include(":examples:example-02")
