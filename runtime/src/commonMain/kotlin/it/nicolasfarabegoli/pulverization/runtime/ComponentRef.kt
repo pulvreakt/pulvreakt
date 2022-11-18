44da503486ab68d66d537c62e72b59565f0b63a3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-18 16:36:28 +0100

    chore: first stub for runtime

---
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/ComponentRef.kt
@@ -0,0 +1,13 @@
+package it.nicolasfarabegoli.pulverization.runtime
+
+interface ComponentRef
+
+class StateRef : ComponentRef
+
+class BehaviourRef : ComponentRef
+
+class CommunicationRef : ComponentRef
+
+class SensorsRef : ComponentRef
+
+class ActuatorRef : ComponentRef
