df93a79a435336c1da0175196b1d917f618512bc
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-18 16:47:12 +0100

    chore: complete some methods

--- runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/PulverizationRuntime.kt
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/PulverizationRuntime.kt
@@ -1,18 +1,14 @@
 package it.nicolasfarabegoli.pulverization.runtime
 
-import it.nicolasfarabegoli.pulverization.component.Context
 import it.nicolasfarabegoli.pulverization.core.ActuatorsComponent
 import it.nicolasfarabegoli.pulverization.core.BehaviourComponent
 import it.nicolasfarabegoli.pulverization.core.CommunicationComponent
 import it.nicolasfarabegoli.pulverization.core.PulverizedComponent
 import it.nicolasfarabegoli.pulverization.core.PulverizedComponentType
 import it.nicolasfarabegoli.pulverization.core.SensorsComponent
-import it.nicolasfarabegoli.pulverization.core.State
 import it.nicolasfarabegoli.pulverization.core.StateComponent
-import it.nicolasfarabegoli.pulverization.core.StateRepresentation
 import it.nicolasfarabegoli.pulverization.dsl.LogicalDeviceConfiguration
 import it.nicolasfarabegoli.pulverization.runtime.dsl.RuntimeDSL
-import kotlinx.coroutines.coroutineScope
 
 class PulverizationRuntime {
 
@@ -52,7 +48,7 @@ class PulverizationRuntime {
 
 internal data class Quadruple<out A, out B, out C, out D>(val first: A, val second: B, val third: C, val fourth: D)
 
-// This sample of code shows the high level API to create a pulverized system
+/* This sample of code shows the high level API to create a pulverized system
 
 data class SR(val i: Int) : StateRepresentation
 class MyState(override val context: Context) : State<SR> {
@@ -71,3 +67,4 @@ suspend fun test(): Unit = coroutineScope {
         withStateLogic(::senseLogic)
     }
 }
+*/
