90436156a1b958d1a02e059fc3460e25c74113e3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-18 16:36:58 +0100

    refactor: remove kclass in favor of adt

--- core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/dsl/ConfigurationModel.kt
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/dsl/ConfigurationModel.kt
@@ -1,26 +1,23 @@
 package it.nicolasfarabegoli.pulverization.dsl
 
-import it.nicolasfarabegoli.pulverization.core.PulverizedComponent
-import kotlin.reflect.KClass
+import it.nicolasfarabegoli.pulverization.core.PulverizedComponentType
 
 sealed interface Tier
 object Cloud : Tier
 object Edge : Tier
 object Device : Tier
 
-data class DeploymentUnit(val deployableComponents: Set<KClass<out PulverizedComponent>>, val where: Tier)
+data class DeploymentUnit(val deployableComponents: Set<PulverizedComponentType>, val where: Tier)
 
 data class LogicalDeviceConfiguration(
     val deviceName: String,
-    val components: Set<KClass<out PulverizedComponent>>,
+    val components: Set<PulverizedComponentType>,
     val deploymentUnits: Set<DeploymentUnit>,
 )
 
 data class DeviceLink(val device: String, val otherDevice: String)
 
-data class DeviceRelationsConfiguration(
-    val links: Set<DeviceLink>,
-)
+data class DeviceRelationsConfiguration(val links: Set<DeviceLink>)
 
 data class PulverizationConfiguration(
     val devicesConfig: Set<LogicalDeviceConfiguration>,
@@ -30,5 +27,8 @@ data class PulverizationConfiguration(
 fun PulverizationConfiguration.getDeviceConfiguration(name: String): LogicalDeviceConfiguration? =
     devicesConfig.firstOrNull { it.deviceName == name }
 
-inline fun <reified C : PulverizedComponent> LogicalDeviceConfiguration.getDeploymentUnit(): DeploymentUnit? =
-    deploymentUnits.firstOrNull { it.deployableComponents.contains(C::class) }
+fun LogicalDeviceConfiguration.getDeploymentUnit(components: Set<PulverizedComponentType>): DeploymentUnit? =
+    deploymentUnits.firstOrNull { it.deployableComponents.containsAll(components) }
+
+fun LogicalDeviceConfiguration.getDeploymentUnit(vararg components: PulverizedComponentType): DeploymentUnit? =
+    getDeploymentUnit(components.toSet())
