2416bd39e76c400c55bce6b56dcccabfeb966034
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-24 10:52:58 +0100

    chore: move into separate all the component ref

--- runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/componentsref/ComponentRef.kt
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/componentsref/ComponentRef.kt
@@ -1,4 +1,4 @@
-package it.nicolasfarabegoli.pulverization.runtime
+package it.nicolasfarabegoli.pulverization.runtime.componentsref
 
 import kotlinx.coroutines.flow.Flow
 
@@ -6,74 +6,4 @@ interface ComponentRef<S> {
     suspend fun sendToComponent(message: S)
     suspend fun receiveFromComponent(): Flow<S>
     suspend fun receiveLastFromComponent(): S
-}
-
-class StateRef<S> : ComponentRef<S> {
-    override suspend fun sendToComponent(message: S) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveFromComponent(): Flow<S> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveLastFromComponent(): S {
-        TODO("Not yet implemented")
-    }
-}
-
-class BehaviourRef<S> : ComponentRef<S> {
-    override suspend fun sendToComponent(message: S) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveFromComponent(): Flow<S> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveLastFromComponent(): S {
-        TODO("Not yet implemented")
-    }
-}
-
-class CommunicationRef<S> : ComponentRef<S> {
-    override suspend fun sendToComponent(message: S) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveFromComponent(): Flow<S> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveLastFromComponent(): S {
-        TODO("Not yet implemented")
-    }
-}
-
-class SensorsRef<S> : ComponentRef<S> {
-    override suspend fun sendToComponent(message: S) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveFromComponent(): Flow<S> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveLastFromComponent(): S {
-        TODO("Not yet implemented")
-    }
-}
-
-class ActuatorRef<S> : ComponentRef<S> {
-    override suspend fun sendToComponent(message: S) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveFromComponent(): Flow<S> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun receiveLastFromComponent(): S {
-        TODO("Not yet implemented")
-    }
 }
