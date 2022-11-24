f6130762e9f2e12ac501286162cedd3657badab4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-24 10:52:58 +0100

    chore: move into separate all the component ref

---
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/componentsref/ActuatorsRef.kt
@@ -0,0 +1,17 @@
+package it.nicolasfarabegoli.pulverization.runtime.componentsref
+
+import kotlinx.coroutines.flow.Flow
+
+class ActuatorsRef<S> : ComponentRef<S> {
+    override suspend fun sendToComponent(message: S) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun receiveFromComponent(): Flow<S> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun receiveLastFromComponent(): S {
+        TODO("Not yet implemented")
+    }
+}
