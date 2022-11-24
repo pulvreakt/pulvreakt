b6513fa00e367af1adf11ac8a9343193f83910cf
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-24 11:54:15 +0100

    chore: add component reference on behaviour

--- runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/componentsref/BehaviourRef.kt
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/componentsref/BehaviourRef.kt
@@ -1,8 +1,9 @@
 package it.nicolasfarabegoli.pulverization.runtime.componentsref
 
+import it.nicolasfarabegoli.pulverization.core.PulverizedComponentType
 import kotlinx.coroutines.flow.Flow
 
-class BehaviourRef<S> : ComponentRef<S> {
+class BehaviourRef<S>(private val relatedWith: PulverizedComponentType) : ComponentRef<S> {
     override suspend fun sendToComponent(message: S) {
         TODO("Not yet implemented")
     }
