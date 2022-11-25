2f34ade5c39b814a81a1d8424b96d8e8a17ecec4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-25 15:40:55 +0100

    chore(build): add json kotlin serialization dependency

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -4,6 +4,7 @@ kotlin {
             dependencies {
                 implementation(project(":core"))
                 implementation("org.jetbrains.kotlin:kotlin-reflect")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
             }
         }
     }
