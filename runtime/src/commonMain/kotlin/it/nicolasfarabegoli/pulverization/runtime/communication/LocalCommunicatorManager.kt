ca5b013a2e1e1aa4fc5663e6c9c5676c08cf31c3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-11-30 16:09:30 +0100

    chore: use flow instead channel

--- runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/communication/LocalCommunicatorManager.kt
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/communication/LocalCommunicatorManager.kt
@@ -1,14 +1,26 @@
 package it.nicolasfarabegoli.pulverization.runtime.communication
 
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 internal object LocalCommunicatorManager {
-    val stateInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val actuatorsInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val sensorsInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val communicationInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val behaviourStateInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val behaviourCommunicationInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val behaviourSensorsInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
-    val behaviourActuatorsInstance: Channel<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { Channel() }
+    val stateInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { MutableSharedFlow(1) }
+    val actuatorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow()
+    }
+    val sensorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { MutableSharedFlow(1) }
+    val communicationInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
+    val behaviourStateInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
+    val behaviourCommunicationInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
+    val behaviourSensorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
+    val behaviourActuatorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
 }
