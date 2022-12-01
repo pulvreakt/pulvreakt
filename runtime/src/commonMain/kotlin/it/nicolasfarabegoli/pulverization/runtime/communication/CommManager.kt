47d630409ad9f293b3f97926edb4ecb6d1a04f50
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-01 11:29:14 +0100

    style: uniform style

--- runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/communication/CommManager.kt
+++ runtime/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/communication/CommManager.kt
@@ -3,11 +3,15 @@ package it.nicolasfarabegoli.pulverization.runtime.communication
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 internal object CommManager {
-    val stateInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { MutableSharedFlow(1) }
+    val stateInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
     val actuatorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
         MutableSharedFlow()
     }
-    val sensorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) { MutableSharedFlow(1) }
+    val sensorsInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        MutableSharedFlow(1)
+    }
     val communicationInstance: MutableSharedFlow<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
         MutableSharedFlow(1)
     }
