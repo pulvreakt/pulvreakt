621e54e955e6448959d6f7889f07c8816eab48f9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-02 16:45:17 +0100

    test: test the runtime in an async fashion

---
+++ runtime/src/commonTest/kotlin/it/nicolasfarabegoli/pulverization/runtime/AsyncScenario.kt
@@ -0,0 +1,64 @@
+package it.nicolasfarabegoli.pulverization.runtime
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
+import it.nicolasfarabegoli.pulverization.core.BehaviourComponent
+import it.nicolasfarabegoli.pulverization.core.CommunicationComponent
+import it.nicolasfarabegoli.pulverization.core.StateComponent
+import it.nicolasfarabegoli.pulverization.dsl.Device
+import it.nicolasfarabegoli.pulverization.dsl.getDeviceConfiguration
+import it.nicolasfarabegoli.pulverization.dsl.pulverizationConfig
+import it.nicolasfarabegoli.pulverization.runtime.dsl.CommPayload
+import it.nicolasfarabegoli.pulverization.runtime.dsl.CommunicationFixture
+import it.nicolasfarabegoli.pulverization.runtime.dsl.FixtureBehaviour
+import it.nicolasfarabegoli.pulverization.runtime.dsl.NoPayload
+import it.nicolasfarabegoli.pulverization.runtime.dsl.PulverizationPlatformScope.Companion.behaviourLogic
+import it.nicolasfarabegoli.pulverization.runtime.dsl.PulverizationPlatformScope.Companion.communicationLogic
+import it.nicolasfarabegoli.pulverization.runtime.dsl.PulverizationPlatformScope.Companion.stateLogic
+import it.nicolasfarabegoli.pulverization.runtime.dsl.StateFixture
+import it.nicolasfarabegoli.pulverization.runtime.dsl.StatePayload
+import it.nicolasfarabegoli.pulverization.runtime.dsl.pulverizationPlatform
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.first
+
+class AsyncScenario : FreeSpec(
+    {
+        "Instantiating the platform" - {
+            "should work as expected" {
+                val config = pulverizationConfig {
+                    logicalDevice("device-1") {
+                        BehaviourComponent and StateComponent and CommunicationComponent deployableOn Device
+                    }
+                }
+                val jobs = pulverizationPlatform(config.getDeviceConfiguration("device-1")!!) {
+                    behaviourLogic(FixtureBehaviour()) { b, sr, cr, _, _ ->
+                        val stateDefer = async { sr.receiveFromComponent().first() }
+                        val commDefer = async { cr.receiveFromComponent().first() }
+                        val st: StatePayload = stateDefer.await()
+                        val comm: CommPayload = commDefer.await()
+                        st shouldBe StatePayload(0)
+                        comm shouldBe CommPayload(3)
+                        val r = b(st, listOf(comm), NoPayload)
+                        sr.sendToComponent(r.newState)
+                        cr.sendToComponent(r.newExport)
+                    }
+                    stateLogic(StateFixture()) { s, br ->
+                        br.sendToComponent(s.get())
+                        val newState: StatePayload = br.receiveFromComponent().first()
+                        newState shouldBe StatePayload(0)
+                        s.update(newState)
+                        return@stateLogic
+                    }
+                    communicationLogic(CommunicationFixture()) { c, br ->
+                        br.sendToComponent(CommPayload(3))
+                        c.send(br.receiveFromComponent().first())
+                        return@communicationLogic
+                    }
+                }.start()
+                jobs.size shouldBe 3
+                jobs.forEach { it.join() }
+            }
+        }
+    },
+)
