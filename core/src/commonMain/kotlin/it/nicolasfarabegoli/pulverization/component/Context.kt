5758515e77bc19eb6ac26d53837971bdfd6b16d4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-05 15:23:19 +0100

    chore: re-introduce context

---
+++ core/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/component/Context.kt
@@ -0,0 +1,13 @@
+package it.nicolasfarabegoli.pulverization.component
+
+import it.nicolasfarabegoli.pulverization.core.DeviceID
+
+/**
+ * TODO.
+ */
+interface Context {
+    /**
+     * TODO.
+     */
+    val id: DeviceID
+}
