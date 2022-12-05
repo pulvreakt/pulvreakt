89eeb2d97f6f735041a31d6f0521b8f43bec6270
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-05 21:54:10 +0100

    chore(build): remove some dependencies

--- rabbitmq-platform/build.gradle.kts
+++ rabbitmq-platform/build.gradle.kts
@@ -2,16 +2,16 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation(project(":core"))
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-                implementation("org.jetbrains.kotlin:kotlin-reflect")
+                implementation(project(":runtime"))
+//                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+//                implementation("org.jetbrains.kotlin:kotlin-reflect")
             }
         }
         jvmMain {
             dependencies {
                 implementation(rootProject.libs.kotlinx.coroutines.reactive)
                 implementation(rootProject.libs.kotlinx.coroutines.reactor)
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+//                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 api("io.projectreactor.rabbitmq:reactor-rabbitmq:1.5.5")
             }
         }
@@ -19,8 +19,8 @@ kotlin {
             dependencies {
                 implementation(rootProject.libs.kotlinx.coroutines.reactive)
                 implementation(rootProject.libs.kotlinx.coroutines.reactor)
-                implementation(rootProject.libs.kotest.extensions.koin)
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+//                implementation(rootProject.libs.kotest.extensions.koin)
+//                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
                 implementation("com.github.fridujo:rabbitmq-mock:1.2.0")
             }
         }
