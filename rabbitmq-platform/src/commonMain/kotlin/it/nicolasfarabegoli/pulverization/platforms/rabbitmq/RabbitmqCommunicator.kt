7fa2650b8407c350bde0bbce256cc62e6a194ec0
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-06 12:32:54 +0100

    chore: align to interface signature

--- rabbitmq-platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/RabbitmqCommunicator.kt
+++ rabbitmq-platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/platforms/rabbitmq/RabbitmqCommunicator.kt
@@ -2,13 +2,14 @@ package it.nicolasfarabegoli.pulverization.platforms.rabbitmq
 
 import it.nicolasfarabegoli.pulverization.runtime.communication.Binding
 import it.nicolasfarabegoli.pulverization.runtime.communication.Communicator
+import it.nicolasfarabegoli.pulverization.runtime.communication.RemotePlace
 import kotlinx.coroutines.flow.Flow
 
 /**
  * TODO.
  */
 class RabbitmqCommunicator : Communicator {
-    override suspend fun setup(binding: Binding) {
+    override suspend fun setup(binding: Binding, remotePlace: RemotePlace?) {
         TODO("Not yet implemented")
     }
 
