48d891be7ad82f38ae7964af85c7728bb8fa9101
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-12 10:17:57 +0100

    chore: dockerized example 03

--- examples/example-03/build.gradle.kts
+++ examples/example-03/build.gradle.kts
@@ -1,4 +1,42 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+fun ShadowJar.genericJarConfig(jarName: String, mainClass: String) {
+    archiveClassifier.set("all")
+    archiveBaseName.set(jarName)
+    archiveVersion.set("")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    exclude("META-INF/*.SF", "META-INF/*.RSA", "META-INF/*.DSA")
+    manifest {
+        attributes("Main-Class" to mainClass)
+    }
+    val main by kotlin.jvm().compilations
+    from(main.output)
+    configurations += main.compileDependencyFiles as Configuration
+    configurations += main.runtimeDependencyFiles as Configuration
+}
+
 kotlin {
+    jvm {
+        apply(plugin = "com.github.johnrengelman.shadow")
+
+        tasks {
+            register("generateJars") {
+                dependsOn("sensorsJar", "behaviourJar", "communicationJar", "stateJar")
+            }
+            register<ShadowJar>("stateJar") {
+                genericJarConfig("state", "example.unit.StateUnitKt")
+            }
+            register<ShadowJar>("behaviourJar") {
+                genericJarConfig("behaviour", "example.unit.BehaviourUnitKt")
+            }
+            register<ShadowJar>("communicationJar") {
+                genericJarConfig("communication", "example.unit.CommunicationUnitKt")
+            }
+            register<ShadowJar>("sensorsJar") {
+                genericJarConfig("sensors", "example.unit.SensorsUnitKt")
+            }
+        }
+    }
     sourceSets {
         commonMain {
             dependencies {
