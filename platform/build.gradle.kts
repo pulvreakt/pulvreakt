843ed28eff97ad48727421059c0ff335841dbd23
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-22 19:26:27 +0100

    chore(deps): cleanup dependencies

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -3,8 +3,7 @@ kotlin {
         commonMain {
             dependencies {
                 api(project(":core"))
-                implementation("org.jetbrains.kotlin:kotlin-reflect")
-                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+                implementation(rootProject.libs.kotlinx.serialization.json)
             }
         }
     }
--- rabbitmq-platform/build.gradle.kts
+++ rabbitmq-platform/build.gradle.kts
@@ -2,27 +2,18 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-//                implementation(project(":core"))
                 implementation(project(":platform"))
-//                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-//                implementation("org.jetbrains.kotlin:kotlin-reflect")
             }
         }
         jvmMain {
             dependencies {
-                implementation(rootProject.libs.kotlinx.coroutines.reactive)
                 implementation(rootProject.libs.kotlinx.coroutines.reactor)
-//                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-                api("io.projectreactor.rabbitmq:reactor-rabbitmq:1.5.5")
+                api(rootProject.libs.rabbitmq.reactor)
             }
         }
         jvmTest {
             dependencies {
-                implementation(rootProject.libs.kotlinx.coroutines.reactive)
                 implementation(rootProject.libs.kotlinx.coroutines.reactor)
-//                implementation(rootProject.libs.kotest.extensions.koin)
-//                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-                implementation("com.github.fridujo:rabbitmq-mock:1.2.0")
             }
         }
     }
