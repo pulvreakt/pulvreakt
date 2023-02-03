ca7bb76c4539e851958dde4310baeb8e47c4cd24
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-02-03 17:20:58 +0100

    chore: publish build scan on failure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+plugins {
+    id("com.gradle.enterprise") version "3.12.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "pulverization-framework"
 
 include(":core")
