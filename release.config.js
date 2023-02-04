ec12b231b8d9a27087c7e93028facae761c4e97b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-02-04 14:56:05 +0100

    chore: use different strategy for compute default version

--- release.config.js
+++ release.config.js
@@ -4,18 +4,12 @@ git push --force origin \${nextRelease.version} || exit 2
 ./gradlew -PstagingRepoId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
 `;
 
-const prepareCmd = `
-echo "Preparing next candidate version"
-SR_VERSION=\${nextRelease.version}
-echo "new-release-version=\${\${SR_VERSION}:-$(git describe --tags)}" >> \$GITHUB_OUTPUT
-`
-
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
   [
     "@semantic-release/exec",
     {
-      "prepareCmd": prepareCmd,
+      "verifyReleaseCmd": "echo ${nextRelease.version} > .next-version",
       "publishCmd": publishCmd,
     }
   ],
