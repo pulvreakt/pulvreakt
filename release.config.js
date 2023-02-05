669bdc922de05de43d6c8515c9a2697f255adf35
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-02-05 09:58:35 +0100

    fix: fix typo that prevent the release of the correct staging repository

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
 git push --force origin \${nextRelease.version} || exit 2
-./gradlew -PstagingRepoId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
+./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
 `;
 
 const config = require('semantic-release-preconfigured-conventional-commits');
