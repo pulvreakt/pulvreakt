0d963a71f0184b77a0cf2fcc4ea6d5fcd1828a7e
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-04-14 17:15:30 +0200

    feat: add facility for creating an execution context

--- platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/context/ExecutionContext.kt
+++ platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/context/ExecutionContext.kt
@@ -12,4 +12,13 @@ interface ExecutionContext : Context {
      * The host on which the system run on.
      */
     val host: Host
+
+    companion object {
+        /**
+         * Creates a new execution context.
+         */
+        fun create(deviceID: String, host: Host): ExecutionContext = ExecutionContextImpl(deviceID, host)
+    }
 }
+
+private class ExecutionContextImpl(override val deviceID: String, override val host: Host) : ExecutionContext
