ebff822a37ae377d0a04b24075e2025b49dbaad5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-04-16 14:46:55 +0200

    fix: cancel an already started fiber before launch a new one

--- platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/spawner/CommunicationSpawnable.kt
+++ platform/src/commonMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/spawner/CommunicationSpawnable.kt
@@ -4,7 +4,6 @@ import it.nicolasfarabegoli.pulverization.core.Communication
 import it.nicolasfarabegoli.pulverization.runtime.componentsref.BehaviourRef
 import it.nicolasfarabegoli.pulverization.runtime.utils.CommunicationLogicType
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
@@ -13,11 +12,12 @@ internal class CommunicationSpawnable<C : Any>(
     private val communication: Communication<C>?,
     private val commLogic: CommunicationLogicType<C>?,
     private val commToBehaviourRef: BehaviourRef<C>,
+    private val scope: CoroutineScope,
 ) : Spawnable {
-    private val scope = CoroutineScope(Dispatchers.Default)
     private var jobRef: Job? = null
 
     override fun spawn(): Job {
+        jobRef?.cancel()
         jobRef = scope.launch {
             communication?.let {
                 it.initialize()
