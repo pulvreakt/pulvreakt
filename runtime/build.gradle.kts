caa9340f031eb97894bd6212051ddde8429f83fd
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-22 13:20:29 +0200

    chore(deps): update all dependencies

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -3,7 +3,6 @@ kotlin {
         commonMain {
             dependencies {
                 api(project(":core"))
-                implementation(rootProject.libs.kotlinx.serialization.json)
             }
         }
     }
