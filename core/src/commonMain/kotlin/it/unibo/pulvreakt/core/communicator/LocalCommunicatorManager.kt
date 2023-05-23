8591ca611cf7b87ba4968551609af454c136e8e9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-23 23:11:13 +0200

    feat: add new communicator interface

---
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/communicator/LocalCommunicatorManager.kt
@@ -0,0 +1,5 @@
+package it.unibo.pulvreakt.core.communicator
+
+internal class LocalCommunicatorManager {
+    fun getLocalCommunicator(sourceComponent: String, destinationComponent: String): Communicator = TODO()
+}
