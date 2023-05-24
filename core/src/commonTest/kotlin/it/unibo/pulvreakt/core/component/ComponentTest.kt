432a191ef62535d9e51820506644481d875188df
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-24 18:26:38 +0200

    refactor: set linked components via method instead property

--- core/src/commonTest/kotlin/it/unibo/pulvreakt/core/component/ComponentTest.kt
+++ core/src/commonTest/kotlin/it/unibo/pulvreakt/core/component/ComponentTest.kt
@@ -1,12 +1,9 @@
 package it.unibo.pulvreakt.core.component
 
 import arrow.core.Either
-import arrow.core.NonEmptySet
 import arrow.core.raise.either
 import arrow.core.right
-import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.koin.KoinExtension
 import io.kotest.matchers.shouldBe
 import it.unibo.pulvreakt.core.communicator.Communicator
 import it.unibo.pulvreakt.core.communicator.Mode
@@ -14,13 +11,12 @@ import it.unibo.pulvreakt.core.unit.NewConfiguration
 import it.unibo.pulvreakt.core.unit.UnitManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 
 class MyComponent : AbstractComponent<Int>() {
     override val name: String = this::class.simpleName!!
-    override val links = NonEmptySet(this, emptySet())
-
     override suspend fun execute(): Either<String, Unit> = either {
         send<Int, MyComponent>(10).bind()
         send<Int, MyComponent>(20).bind()
@@ -43,13 +39,15 @@ class FakeUnitManager : UnitManager {
 }
 
 class ComponentTest : FreeSpec(), KoinTest {
-    private val koinModule = module {
-        factory<Communicator> { FakeCommunicator() }
-        single<UnitManager> { FakeUnitManager() }
-    }
-    override fun extensions(): List<Extension> = listOf(KoinExtension(koinModule))
-
     init {
+        startKoin {
+            modules(
+                module {
+                    factory<Communicator> { FakeCommunicator() }
+                    single<UnitManager> { FakeUnitManager() }
+                },
+            )
+        }
         "A Component" - {
             "should send messages to other linked components" {
                 val myComponent = MyComponent().apply { initialize() }
