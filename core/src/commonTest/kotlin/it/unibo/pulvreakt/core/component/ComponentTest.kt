08372480a3d9a323d90a7fe08cb2eb045e6c85a7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-26 17:40:28 +0200

    fix: use test-specific module for koin

--- core/src/commonTest/kotlin/it/unibo/pulvreakt/core/component/ComponentTest.kt
+++ core/src/commonTest/kotlin/it/unibo/pulvreakt/core/component/ComponentTest.kt
@@ -3,17 +3,18 @@ package it.unibo.pulvreakt.core.component
 import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.right
+import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import it.unibo.pulvreakt.core.communicator.Communicator
 import it.unibo.pulvreakt.core.communicator.Mode
 import it.unibo.pulvreakt.core.unit.NewConfiguration
 import it.unibo.pulvreakt.core.unit.UnitManager
-import it.unibo.pulvreakt.core.utils.PulvreaktKoinContext
+import it.unibo.pulvreakt.utils.KoinExtension
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
-import org.koin.dsl.koinApplication
 import org.koin.dsl.module
+import org.koin.test.KoinTest
 
 class MyComponent : AbstractComponent<Int>() {
     override val name: String = this::class.simpleName!!
@@ -38,14 +39,15 @@ class FakeUnitManager : UnitManager {
     override suspend fun finalize(): Either<String, Unit> = Unit.right()
 }
 
-class ComponentTest : FreeSpec() {
+class ComponentTest : FreeSpec(), KoinTest {
     private val koinModule = module {
         factory<Communicator> { FakeCommunicator() }
         single<UnitManager> { FakeUnitManager() }
     }
 
+    override fun extensions(): List<Extension> = listOf(KoinExtension(koinModule))
+
     init {
-        PulvreaktKoinContext.koinApp = koinApplication { modules(koinModule) }
         "A Component" - {
             "should send messages to other linked components" {
                 val myComponent = MyComponent().apply {
