43a6d686930bf9cbd3cc9e54d992f7c534cddb11
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-26 17:40:28 +0200

    fix: use test-specific module for koin

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/utils/PulvreaktKoinContext.kt
+++
@@ -1,9 +0,0 @@
-package it.unibo.pulvreakt.core.utils
-
-import org.koin.core.KoinApplication
-import kotlin.native.concurrent.ThreadLocal
-
-@ThreadLocal
-internal object PulvreaktKoinContext {
-    var koinApp: KoinApplication? = null
-}
