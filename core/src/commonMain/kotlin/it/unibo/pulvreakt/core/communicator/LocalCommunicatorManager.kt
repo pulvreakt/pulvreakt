720864dd0025064a8dd3dcbe68cf5f1dd8751ae8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-28 12:22:23 +0200

    fix: solved a problem which lead to resune wrongly the same flow for inbond/outbound messages

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/communicator/LocalCommunicatorManager.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/communicator/LocalCommunicatorManager.kt
@@ -3,10 +3,10 @@ package it.unibo.pulvreakt.core.communicator
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 internal class LocalCommunicatorManager {
-    private var sharedFlowRegistry: Map<Set<String>, MutableSharedFlow<ByteArray>> = emptyMap()
+    private var sharedFlowRegistry: Map<Pair<String, String>, MutableSharedFlow<ByteArray>> = emptyMap()
 
     fun getLocalCommunicator(sourceComponent: String, destinationComponent: String): Communicator {
-        val key = setOf(sourceComponent, destinationComponent)
+        val key = sourceComponent to destinationComponent
         val sharedFlow = sharedFlowRegistry[key] ?: MutableSharedFlow()
         sharedFlowRegistry += key to sharedFlow
         return LocalCommunicator(sharedFlow)
