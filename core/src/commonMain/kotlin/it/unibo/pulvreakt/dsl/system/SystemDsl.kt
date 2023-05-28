e8d75365d58fe49853ec28560228afc383cb7d71
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-28 22:34:05 +0200

    feat: new stub for system DSL

---
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/system/SystemDsl.kt
@@ -0,0 +1,5 @@
+package it.unibo.pulvreakt.dsl.system
+
+import it.unibo.pulvreakt.dsl.model.SystemSpecification
+
+fun pulverizedSystem(config: SystemSpecificationScope.() -> Unit): SystemSpecification = TODO()
