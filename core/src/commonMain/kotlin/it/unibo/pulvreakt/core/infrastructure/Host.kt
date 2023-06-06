850f6a10fdc5a74a7d1e086c6b7db5a8b4debea4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-06-06 18:45:05 +0200

    feat: make construction via delegation

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/infrastructure/Host.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/infrastructure/Host.kt
@@ -1,6 +1,33 @@
 package it.unibo.pulvreakt.core.infrastructure
 
+import arrow.core.NonEmptySet
+import arrow.core.nonEmptySetOf
+import it.unibo.pulvreakt.dsl.system.model.Capability
+import kotlin.reflect.KProperty
+
 /**
  * Represents the host of the available infrastructure.
  */
-interface Host
+sealed interface Host {
+    val hostname: String
+    val capabilities: NonEmptySet<Capability>
+
+    companion object {
+        operator fun invoke(hostname: String, capabilities: NonEmptySet<Capability>): Host = HostImpl(hostname, capabilities)
+    }
+
+    private data class HostImpl(override val hostname: String, override val capabilities: NonEmptySet<Capability>) : Host {
+        override fun toString(): String = hostname
+    }
+}
+
+class HostDelegate(private val capabilities: NonEmptySet<Capability>) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Host = Host(property.name, capabilities)
+
+    companion object {
+        operator fun invoke(firstCapability: Capability, vararg capabilities: Capability): HostDelegate =
+            HostDelegate(nonEmptySetOf(firstCapability, *capabilities))
+        operator fun invoke(capabilities: NonEmptySet<Capability>): HostDelegate =
+            HostDelegate(capabilities)
+    }
+}
