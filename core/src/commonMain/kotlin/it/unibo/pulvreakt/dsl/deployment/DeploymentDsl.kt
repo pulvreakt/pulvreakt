6a2e36ddcfcaa18a371d225bd6ea5a11e185fcc3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-06-06 18:45:35 +0200

    feat: consolidated deployment dsl

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/deployment/DeploymentDsl.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/deployment/DeploymentDsl.kt
@@ -2,6 +2,9 @@ package it.unibo.pulvreakt.dsl.deployment
 
 import arrow.core.Either
 import arrow.core.NonEmptyList
+import arrow.core.raise.either
+import arrow.core.raise.ensureNotNull
+import arrow.core.raise.zipOrAccumulate
 import it.unibo.pulvreakt.core.infrastructure.Host
 import it.unibo.pulvreakt.dsl.deployment.model.DeploymentSpecification
 import it.unibo.pulvreakt.dsl.system.model.SystemSpecification
@@ -14,5 +17,10 @@ fun pulverizationRuntime(
     availableHosts: Set<Host>,
     systemSpecification: SystemSpecification,
     config: DeploymentDslScope.() -> Unit,
-): Either<NonEmptyList<String>, DeploymentSpecification> =
-    DeploymentDslScope(deviceName, availableHosts, systemSpecification).apply(config).generate()
+): Either<NonEmptyList<String>, DeploymentSpecification> = either {
+    val deviceConfiguration = systemSpecification.logicalDevices.firstOrNull { it.deviceName == deviceName }
+    zipOrAccumulate(
+        { ensureNotNull(deviceConfiguration) { "No device with name '$deviceName' found in the system configuration" } },
+        { DeploymentDslScope(availableHosts, deviceConfiguration!!).apply(config).generate().bind() },
+    ) { _, deploymentSpecification -> deploymentSpecification }
+}
--- core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/deployment/OnDeviceScope.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/deployment/OnDeviceScope.kt
@@ -2,24 +2,47 @@ package it.unibo.pulvreakt.dsl.deployment
 
 import arrow.core.Either
 import arrow.core.NonEmptyList
-import arrow.core.mapOrAccumulate
+import arrow.core.nonEmptyListOf
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import it.unibo.pulvreakt.core.component.ComponentType
+import it.unibo.pulvreakt.core.infrastructure.Host
 import it.unibo.pulvreakt.core.reconfiguration.event.ReconfigurationEvent
 import it.unibo.pulvreakt.dsl.deployment.model.DeviceReconfigurationRule
+import it.unibo.pulvreakt.dsl.deployment.model.NewConfiguration
+import it.unibo.pulvreakt.dsl.system.model.LogicalDeviceSpecification
 
 /**
  * Scope for the reconfiguration of a device.
  */
-class OnDeviceScope {
-    private val reconfigurationRules = mutableListOf<Either<String, DeviceReconfigurationRule>>()
+class OnDeviceScope(private val logicalDeviceSpecification: LogicalDeviceSpecification) {
+    private val reconfigurationRules = mutableListOf<Either<NonEmptyList<String>, DeviceReconfigurationRule>>()
 
     /**
-     * Specifies the reconfiguration associated to the [ReconfigurationEvent].
+     * Specifies the [newConfiguration] associated to the [ReconfigurationEvent].
      */
-    infix fun ReconfigurationEvent<*>.reconfigures(config: ReconfigurationComponentScope.() -> Unit) {
-        val reconfigurationScope = ReconfigurationComponentScope().apply(config)
-        val reconfiguration = reconfigurationScope.generate()
-        reconfigurationRules.add(reconfiguration.map { DeviceReconfigurationRule(this, it) })
+    infix fun ReconfigurationEvent<*>.reconfigures(newConfiguration: NewConfiguration) {
+        val (componentType, host) = newConfiguration
+        val requiredCapabilities = logicalDeviceSpecification.componentsRequiredCapabilities[componentType] ?: emptySet()
+        val isValidHost = requiredCapabilities.intersect(host.capabilities).isNotEmpty()
+        val newReconfiguration = either {
+            ensure(isValidHost) {
+                nonEmptyListOf(
+                    """
+                        $componentType cannot be moved to $host.
+                         Because $componentType requires at least one of the following capabilities: $requiredCapabilities.
+                    """.trimIndent(),
+                )
+            }
+            DeviceReconfigurationRule(this@reconfigures, newConfiguration)
+        }
+        reconfigurationRules.add(newReconfiguration)
     }
 
-    internal fun generate(): Either<NonEmptyList<String>, List<DeviceReconfigurationRule>> = reconfigurationRules.mapOrAccumulate { e -> e.bind() }
+    /**
+     * Utility method for create a [NewConfiguration].
+     */
+    infix fun ComponentType<*>.movesTo(host: Host): NewConfiguration = this to host
+
+    internal fun generate(): Either<NonEmptyList<String>, List<DeviceReconfigurationRule>> = either { reconfigurationRules.bindAll() }
 }
