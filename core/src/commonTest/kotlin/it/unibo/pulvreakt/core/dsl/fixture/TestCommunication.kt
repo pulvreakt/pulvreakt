86385751f9b954ea7ce3916ca7e2cee32eda285f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-06-29 17:42:47 +0200

    test: improve dsl test suite

---
+++ core/src/commonTest/kotlin/it/unibo/pulvreakt/core/dsl/fixture/TestCommunication.kt
@@ -0,0 +1,53 @@
+package it.unibo.pulvreakt.core.dsl.fixture
+
+import arrow.core.Either
+import it.unibo.pulvreakt.core.communicator.Communicator
+import it.unibo.pulvreakt.core.communicator.Mode
+import it.unibo.pulvreakt.core.component.ComponentRef
+import it.unibo.pulvreakt.core.reconfiguration.ReconfigurationMessage
+import it.unibo.pulvreakt.core.reconfiguration.Reconfigurator
+import kotlinx.coroutines.flow.Flow
+import org.kodein.di.DI
+
+class TestCommunicator : Communicator {
+    override suspend fun communicatorSetup(source: ComponentRef<*>, destination: ComponentRef<*>): Either<String, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setMode(mode: Mode) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun sendToComponent(message: ByteArray): Either<String, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun receiveFromComponent(): Either<String, Flow<ByteArray>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun initialize(): Either<String, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun finalize(): Either<String, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupInjector(kodein: DI) {
+        TODO("Not yet implemented")
+    }
+
+    override val di: DI
+        get() = TODO("Not yet implemented")
+}
+
+class TestReconfigurator : Reconfigurator {
+    override suspend fun reconfigure(newConfiguration: ReconfigurationMessage) {
+        TODO("Not yet implemented")
+    }
+
+    override fun receiveConfiguration(): Flow<ReconfigurationMessage> {
+        TODO("Not yet implemented")
+    }
+}
