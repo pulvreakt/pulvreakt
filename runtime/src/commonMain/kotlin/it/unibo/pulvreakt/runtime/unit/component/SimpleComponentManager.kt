005d34c7000f79bb84efd4afa4455b6fa5a74574
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-06-29 17:57:07 +0200

    chore: fix import path

--- runtime/src/commonMain/kotlin/it/unibo/pulvreakt/runtime/unit/component/SimpleComponentManager.kt
+++ runtime/src/commonMain/kotlin/it/unibo/pulvreakt/runtime/unit/component/SimpleComponentManager.kt
@@ -4,12 +4,12 @@ import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import arrow.core.right
+import io.github.oshai.kotlinlogging.KotlinLogging
 import it.unibo.pulvreakt.core.component.Component
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import mu.KotlinLogging
 
 internal class SimpleComponentManager : ComponentManager {
     private var componentContainer = emptyMap<Component, Job?>()
