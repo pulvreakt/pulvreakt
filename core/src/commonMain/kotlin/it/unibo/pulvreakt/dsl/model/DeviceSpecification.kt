8cc0a0a6c7f296e13651f95d4a89ff7f900f4ab7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-06-30 15:54:48 +0200

    feat: implement canonical pulverization device

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/model/DeviceSpecification.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/model/DeviceSpecification.kt
@@ -3,5 +3,6 @@ package it.unibo.pulvreakt.dsl.model
 data class DeviceSpecification(
     val deviceName: String,
     val componentsConfiguration: ComponentsGraph,
+    val requiredCapabilities: RequiredCapabilities,
     val runtimeConfiguration: DeviceRuntimeConfiguration,
 )
