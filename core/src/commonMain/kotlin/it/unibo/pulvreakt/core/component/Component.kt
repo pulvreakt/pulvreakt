b4a2bc2e04943182ec4d586a9d388abbd25c1962
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-06 18:14:06 +0200

    docs: add kdoc to component

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/component/Component.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/component/Component.kt
@@ -11,16 +11,32 @@ import kotlinx.serialization.KSerializer
  * Represents a component of the system.
  */
 interface Component<T : Any> : Initializable<ComponentError>, PulvreaktInjected {
+    /**
+     * Setup all the other [components] to which this component will communicate.
+     */
     fun setupWiring(vararg components: ComponentRef<*>)
 
+    /**
+     * Returns a symbolic reference to this component.
+     * This method is used internally by the runtime and should not be called by the user.
+     *
+     * To get a reference to a component from a type, use [ComponentRef.create].
+     */
     fun getRef(): ComponentRef<T> = ComponentRef.create(this)
 
+    /**
+     * Sends a [message] [toComponent] by serializing it with the given [serializer].
+     */
     suspend fun send(
         toComponent: ComponentRef<*>,
         message: T,
         serializer: KSerializer<T>,
     ): Either<ComponentError, Unit>
 
+    /**
+     * Receives a message [fromComponent] by deserializing it with the given [serializer].
+     * @param P the type of the message to receive.
+     */
     suspend fun <P : Any> receive(
         fromComponent: ComponentRef<P>,
         serializer: KSerializer<P>,
