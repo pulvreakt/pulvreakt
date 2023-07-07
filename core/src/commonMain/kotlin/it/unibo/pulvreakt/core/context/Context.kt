f7ccbb48558115b376f0fbfd46a60cc0509d7e77
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-07 11:07:35 +0200

    docs(context): add missing kdoc

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/context/Context.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/context/Context.kt
@@ -4,5 +4,8 @@ package it.unibo.pulvreakt.core.context
  * Represents the context of the deployment unit.
  */
 interface Context {
+    /**
+     * The ID of the device.
+     */
     val deviceId: Int
 }
