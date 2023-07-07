621f38b96a2e5d3215c0ddcaad4b7f662dfe1062
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-07 11:10:51 +0200

    docs(protocol): add missing kdoc

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/Protocol.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/Protocol.kt
@@ -5,13 +5,31 @@ import it.unibo.pulvreakt.core.protocol.errors.ProtocolError
 import it.unibo.pulvreakt.core.utils.Initializable
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Represents the entity that the [Protocol] is communicating with.
+ * An [Entity] is characterized by a [entityName] and an optional [id].
+ * The [id] is used to distinguish between multiple entities with the same [entityName].
+ * The [metadata] is used to store additional information about the entity.
+ */
 data class Entity(val entityName: String, val id: String? = null, val metadata: Map<String, String> = emptyMap())
 
 /**
  * Represents the low-level operations needed to communicate with another entity.
+ * The communication is done through a channel that is set up by the [Protocol].
  */
 interface Protocol : Initializable<ProtocolError> {
+    /**
+     * Sets up the communication channel with the given [entity].
+     */
     suspend fun setupChannel(entity: Entity)
+
+    /**
+     * Writes the given [message] to the channel of the given [to] entity.
+     */
     suspend fun writeToChannel(to: Entity, message: ByteArray): Either<ProtocolError, Unit>
+
+    /**
+     * Reads from the channel of the given [from] entity.
+     */
     fun readFromChannel(from: Entity): Either<ProtocolError, Flow<ByteArray>>
 }
--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/errors/ProtocolError.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/errors/ProtocolError.kt
@@ -1,7 +1,14 @@
 package it.unibo.pulvreakt.core.protocol.errors
 
 import it.unibo.pulvreakt.core.protocol.Entity
+import it.unibo.pulvreakt.core.protocol.Protocol
 
+/**
+ * Represents all the possible errors that a [Protocol] can raise.
+ */
 sealed interface ProtocolError {
+    /**
+     * Error raised when the [Protocol] tries to communicate with an [entity] that is not registered.
+     */
     data class EntityNotRegistered(val entity: Entity) : ProtocolError
 }
