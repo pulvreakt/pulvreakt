b1a464d38dda90ed47cffe8fbdda02c2ed4d21d1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-07 11:07:48 +0200

    docs(host): add missing kdoc

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/infrastructure/Host.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/infrastructure/Host.kt
@@ -3,35 +3,36 @@ package it.unibo.pulvreakt.core.infrastructure
 import arrow.core.NonEmptySet
 import arrow.core.nonEmptySetOf
 import it.unibo.pulvreakt.dsl.model.Capability
-import kotlin.reflect.KProperty
 
 /**
  * Represents the host of the available infrastructure.
  */
 sealed interface Host {
+    /**
+     * The hostname of the host.
+     */
     val hostname: String
+
+    /**
+     * The set of [Capability] of the host.
+     */
     val capabilities: NonEmptySet<Capability>
 
     companion object {
+        /**
+         * Creates a [Host] with the given [hostname] and [capabilities] as a non-empty set.
+         */
         operator fun invoke(hostname: String, capabilities: NonEmptySet<Capability>): Host =
             HostImpl(hostname, capabilities)
+
+        /**
+         * Creates a [Host] with the given [hostname] and [capability].
+         */
         operator fun invoke(hostname: String, capability: Capability, vararg otherCapabilities: Capability): Host =
             HostImpl(hostname, nonEmptySetOf(capability, *otherCapabilities))
     }
 
     private data class HostImpl(override val hostname: String, override val capabilities: NonEmptySet<Capability>) : Host {
         override fun toString(): String = hostname
-    }
-}
-
-class HostDelegate(private val capabilities: NonEmptySet<Capability>) {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): Host = Host(property.name, capabilities)
-
-    companion object {
-        operator fun invoke(firstCapability: Capability, vararg capabilities: Capability): HostDelegate =
-            HostDelegate(nonEmptySetOf(firstCapability, *capabilities))
-
-        operator fun invoke(capabilities: NonEmptySet<Capability>): HostDelegate =
-            HostDelegate(capabilities)
     }
 }
