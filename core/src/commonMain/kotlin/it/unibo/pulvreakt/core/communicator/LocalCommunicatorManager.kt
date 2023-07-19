ff1a09481b628058899180c2fe872b92ef6732d5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-19 14:16:56 +0200

    fix: use two channel for communication

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/communicator/LocalCommunicatorManager.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/communicator/LocalCommunicatorManager.kt
@@ -1,14 +1,21 @@
 package it.unibo.pulvreakt.core.communicator
 
+import it.unibo.pulvreakt.core.component.ComponentRef
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 internal class LocalCommunicatorManager {
-    private var sharedFlowRegistry: Map<Pair<String, String>, MutableSharedFlow<ByteArray>> = emptyMap()
+    private var sharedFlowRegistry:
+        Map<Pair<ComponentRef, ComponentRef>, Pair<MutableSharedFlow<ByteArray>, MutableSharedFlow<ByteArray>>> = emptyMap()
 
-    fun getLocalCommunicator(sourceComponent: String, destinationComponent: String): Communicator {
+    fun getLocalCommunicator(sourceComponent: ComponentRef, destinationComponent: ComponentRef): Communicator {
         val key = sourceComponent to destinationComponent
-        val sharedFlow = sharedFlowRegistry[key] ?: MutableSharedFlow()
-        sharedFlowRegistry += key to sharedFlow
-        return LocalCommunicator(sharedFlow)
+        val reverseKey = destinationComponent to sourceComponent
+        val (inFlow, outFlow) = sharedFlowRegistry[key] ?: run {
+            val (inF, outF) = MutableSharedFlow<ByteArray>(1) to MutableSharedFlow<ByteArray>(1)
+            sharedFlowRegistry += key to (inF to outF)
+            sharedFlowRegistry += reverseKey to (outF to inF)
+            inF to outF // Return the newly created
+        }
+        return LocalCommunicator(inFlow, outFlow)
     }
 }
