feba2f1e99fc99f69c154e8187d042999edac4a2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-20 17:26:05 +0200

    chore: add utility function

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/component/pulverisation/Behaviour.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/component/pulverisation/Behaviour.kt
@@ -1,10 +1,13 @@
-package it.unibo.pulvreakt.core.component.pulverisation
+package it.unibo.pulvreakt.core.component
 
 import arrow.core.Either
 import arrow.core.raise.either
-import it.unibo.pulvreakt.core.component.ComponentRef
-import it.unibo.pulvreakt.core.component.ComponentType
 import it.unibo.pulvreakt.core.component.errors.ComponentError
+import it.unibo.pulvreakt.core.component.pulverisation.AbstractPulverisedComponent
+import it.unibo.pulvreakt.core.component.pulverisation.communication.CommunicationPayload
+import it.unibo.pulvreakt.core.component.pulverisation.state.GetState
+import it.unibo.pulvreakt.core.component.pulverisation.state.SetState
+import it.unibo.pulvreakt.core.component.pulverisation.state.StateOps
 import it.unibo.pulvreakt.core.component.time.TimeDistribution
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
@@ -31,58 +34,78 @@ abstract class Behaviour<State : Any, Comm : Any, Sensors : Any, Actuators : Any
      * Execute the behaviour logic with the given [state], [comm] and [sensors].
      * @return the new state, the communication to be sent and the prescriptive actions to be performed.
      */
-    abstract operator fun invoke(state: State, comm: List<Comm>, sensors: Sensors): BehaviourOutput<State, Comm, Actuators>
+    abstract operator fun invoke(state: State?, comm: List<Comm>, sensors: Sensors?): BehaviourOutput<State, Comm, Actuators>
 
     final override fun getRef(): ComponentRef = myRef
 
     override suspend fun execute(): Either<ComponentError, Unit> = coroutineScope {
         either {
-            val stateRef = getComponentByType(ComponentType.State).bind()
-            val commRef = getComponentByType(ComponentType.Communication).bind()
-            val sensorsRef = getComponentByType(ComponentType.Sensor).bind()
-            val actuatorsRef = getComponentByType(ComponentType.Actuator).bind()
+            val stateRef = getComponentByTypeOrNull(ComponentType.State)
+            val commRef = getComponentByTypeOrNull(ComponentType.Communication)
+            val sensorsRef = getComponentByTypeOrNull(ComponentType.Sensor)
+            val actuatorsRef = getComponentByTypeOrNull(ComponentType.Actuator)
 
             var receivedNeighboursMessages = listOf<CommunicationPayload<Comm>>()
             var lastSensorsRead: Sensors? = null
 
-            val commJob = launch {
-                val communicationFlow = receive(commRef, commSerializer).bind()
-                communicationFlow.collect {
-                    receivedNeighboursMessages = receivedNeighboursMessages.filter { message -> message.deviceId != message.deviceId } + it
+            val commJob = executeIfNotNull(commRef) { ref ->
+                launch {
+                    val communicationFlow = receive(ref, commSerializer).bind()
+                    communicationFlow.collect {
+                        receivedNeighboursMessages = receivedNeighboursMessages.filter { message -> message.deviceId != message.deviceId } + it
+                    }
                 }
             }
 
-            val sensorsJob = launch {
-                val sensorsFlow = receive(sensorsRef, sensorsSerializer).bind()
-                sensorsFlow.collect { lastSensorsRead = it }
+            val sensorsJob = executeIfNotNull(sensorsRef) { ref ->
+                launch { receive(ref, sensorsSerializer).bind().collect { lastSensorsRead = it } }
             }
 
             // Loop with a user-specific policy
             while (!timeDistribution.isCompleted()) {
-                send(stateRef, GetState as StateOps<State>, stateSerializer).bind()
-                when (val state = receive(stateRef, stateSerializer).bind().first()) {
-                    is SetState -> {
-                        if (lastSensorsRead != null) {
-                            val (newState, newComm, newActuators) = invoke(
-                                state.content,
-                                receivedNeighboursMessages.map { it.payload },
-                                lastSensorsRead!!,
-                            )
-                            send(stateRef, SetState(newState), stateSerializer).bind()
-                            send(commRef, CommunicationPayload(deviceId, newComm), commSerializer).bind()
-                            send(actuatorsRef, newActuators, actuatorsSerializer).bind()
-                        }
+                val stateContent = executeIfNotNull(stateRef) { ref ->
+                    send(ref, GetState, stateSerializer).bind()
+                    when (val state = receive(ref, stateSerializer).bind().first()) {
+                        is SetState -> state.content
+                        is GetState -> null
                     }
-
-                    is GetState -> Unit
                 }
+
+                val (newState, newComm, newActuators) = invoke(
+                    stateContent,
+                    receivedNeighboursMessages.map { it.payload },
+                    lastSensorsRead,
+                )
+
+                executeIfNotNull(newState, stateRef) { state, ref ->
+                    send(ref, SetState(state), stateSerializer).bind()
+                }
+                executeIfNotNull(newComm, commRef) { comm, ref ->
+                    send(ref, CommunicationPayload(deviceId, comm), commSerializer).bind()
+                }
+                executeIfNotNull(newActuators, actuatorsRef) { actuators, ref ->
+                    send(ref, actuators, actuatorsSerializer).bind()
+                }
+
                 delay(timeDistribution.nextTimeInstant())
             }
-
-            commJob.cancelAndJoin()
-            sensorsJob.cancelAndJoin()
+            commJob?.cancelAndJoin()
+            sensorsJob?.cancelAndJoin()
         }
     }
+
+    /**
+     * Executes the given [block] if the given [value] is not null.
+     * @return the result of the block execution or null if the value is null.
+     */
+    private suspend fun <T, P> executeIfNotNull(value: T?, block: suspend (T) -> P): P? = value?.let { block(it) }
+
+    /**
+     * Executes the given [block] if the given [value1] and [value2] are not null.
+     * @return the result of the block execution or null if one of the values is null.
+     */
+    private suspend fun <T, R, P> executeIfNotNull(value1: T?, value2: R?, block: suspend (T, R) -> P): P? =
+        value1?.let { v1 -> value2?.let { v2 -> block(v1, v2) } }
 }
 
 /**
@@ -91,7 +114,7 @@ abstract class Behaviour<State : Any, Comm : Any, Sensors : Any, Actuators : Any
  * and [actuators] is the prescriptive actions to be performed.
  */
 data class BehaviourOutput<State : Any, Comm : Any, Actuators : Any>(
-    val state: State,
-    val comm: Comm,
-    val actuators: Actuators,
+    val state: State?,
+    val comm: Comm?,
+    val actuators: Actuators?,
 )
