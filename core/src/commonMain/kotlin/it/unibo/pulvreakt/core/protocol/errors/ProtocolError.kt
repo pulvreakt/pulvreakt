41de487199dd2a8377bb77d78c0bb0160ef6430d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-24 11:24:44 +0200

    feat: add new error type for protocol

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/errors/ProtocolError.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/core/protocol/errors/ProtocolError.kt
@@ -11,4 +11,9 @@ sealed interface ProtocolError {
      * Error raised when the [Protocol] tries to communicate with an [entity] that is not registered.
      */
     data class EntityNotRegistered(val entity: Entity) : ProtocolError
+
+    /**
+     * Wrap the [exception] raised by the [Protocol] in a [ProtocolError].
+     */
+    data class ProtocolException(val exception: Throwable) : ProtocolError
 }
