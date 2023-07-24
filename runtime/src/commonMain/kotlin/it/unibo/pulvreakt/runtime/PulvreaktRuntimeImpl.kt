2eee44199515ce57bdd8ba68759f5765f4867860
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-24 10:04:53 +0200

    chore: attach new reconfigurator stub to the runtime

--- runtime/src/commonMain/kotlin/it/unibo/pulvreakt/runtime/PulvreaktRuntimeImpl.kt
+++ runtime/src/commonMain/kotlin/it/unibo/pulvreakt/runtime/PulvreaktRuntimeImpl.kt
@@ -19,7 +19,10 @@ import it.unibo.pulvreakt.runtime.component.SimpleComponentManager
 import it.unibo.pulvreakt.runtime.errors.DeviceConfigurationNotFound
 import it.unibo.pulvreakt.runtime.errors.RuntimeError
 import it.unibo.pulvreakt.runtime.errors.UnitManagerNotInitialized
+import it.unibo.pulvreakt.runtime.errors.UnitReconfiguratorNotInitialized
 import it.unibo.pulvreakt.runtime.errors.WrapUnitManagerError
+import it.unibo.pulvreakt.runtime.errors.WrapUnitReconfiguratorError
+import it.unibo.pulvreakt.runtime.reconfigurator.UnitReconfigurator
 import it.unibo.pulvreakt.runtime.unit.UnitManager
 import org.kodein.di.DI
 import org.kodein.di.bind
@@ -34,18 +37,23 @@ internal class PulvreaktRuntimeImpl(
 ) : PulvreaktRuntime {
 
     private lateinit var unitManager: UnitManager
+    private lateinit var unitReconfigurator: UnitReconfigurator
     private val logger = KotlinLogging.logger("PulvreaktRuntime")
 
     override suspend fun start(): Either<RuntimeError, Unit> = either {
         logger.debug { "Start Pulvreakt runtime" }
         ensure(::unitManager.isInitialized) { UnitManagerNotInitialized }
+        ensure(::unitReconfigurator.isInitialized) { UnitReconfiguratorNotInitialized }
         unitManager.start().mapLeft { WrapUnitManagerError(it) }.bind()
+        // unitReconfigurator.start()
     }
 
     override suspend fun stop(): Either<RuntimeError, Unit> = either {
         logger.debug { "Stop Pulvreakt runtime" }
         ensure(::unitManager.isInitialized) { UnitManagerNotInitialized }
+        ensure(::unitReconfigurator.isInitialized) { UnitReconfiguratorNotInitialized }
         unitManager.stop().mapLeft { WrapUnitManagerError(it) }.bind()
+        // unitReconfigurator.stop()
     }
 
     override suspend fun initialize(): Either<RuntimeError, Unit> = either {
@@ -62,8 +70,8 @@ internal class PulvreaktRuntimeImpl(
             bind<Protocol> { provider { config.protocol } }
             bind<Communicator> { provider { CommunicatorImpl() } }
             bind<LocalCommunicatorManager> { singleton { LocalCommunicatorManager() } }
-            bind<Reconfigurator> { singleton { TODO() } }
-            bind<ComponentModeReconfigurator> { singleton { TODO() } }
+            bind<Reconfigurator> { singleton { Reconfigurator() } }
+            bind<ComponentModeReconfigurator> { singleton { ComponentModeReconfigurator() } }
             bind<ComponentManager> { singleton { SimpleComponentManager() } }
             bind<Context> { provider { Context(id, host) } }
         }
@@ -73,6 +81,10 @@ internal class PulvreaktRuntimeImpl(
         unitManager = UnitManager(deviceConfiguration)
         unitManager.setupInjector(diModule)
         unitManager.initialize().mapLeft { WrapUnitManagerError(it) }.bind()
+
+        unitReconfigurator = UnitReconfigurator()
+        unitReconfigurator.setupInjector(diModule)
+        unitReconfigurator.initialize().mapLeft { err -> WrapUnitReconfiguratorError(err) }.bind()
 
         logger.debug { "PulvreaktRuntime initialized" }
     }
