8de9d838fc9d1572fb20e89d4916b5695d2f353b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-24 10:04:30 +0200

    feat: add runtime reconfigurator stub

---
+++ runtime/src/commonMain/kotlin/it/unibo/pulvreakt/runtime/reconfigurator/errors/UnitReconfiguratorError.kt
@@ -0,0 +1,6 @@
+package it.unibo.pulvreakt.runtime.reconfigurator.errors
+
+/**
+ * Represents an error that can occur during the reconfiguration of a unit.
+ */
+sealed interface UnitReconfiguratorError
