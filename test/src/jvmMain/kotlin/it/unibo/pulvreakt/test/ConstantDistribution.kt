39c3e61172638f3c27f7731b0ae0b0dd41e10225
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-07-28 12:31:40 +0200

    chore: remove test example

--- test/src/jvmMain/kotlin/it/unibo/pulvreakt/test/ConstantDistribution.kt
+++
@@ -1,8 +0,0 @@
-package it.unibo.pulvreakt.test
-
-import it.unibo.pulvreakt.core.component.time.TimeDistribution
-
-class ConstantDistribution(private val time: Long) : TimeDistribution {
-    override fun nextTimeInstant(): Long = time
-    override fun isCompleted(): Boolean = false
-}
