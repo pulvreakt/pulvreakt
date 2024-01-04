9013a835e55410ac3c38bef18e1a3d22cd185225
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-01-04 11:00:06 +0100

    docs: improve docs with an example for DSL entrypoint

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/PulverizationDsl.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/dsl/PulverizationDsl.kt
@@ -6,7 +6,24 @@ import it.unibo.pulvreakt.dsl.errors.ConfigurationError
 import it.unibo.pulvreakt.dsl.model.PulvreaktConfiguration
 
 /**
- * Entrypoint for the pulverization DSL.
+ * Entrypoint for the pulverization DSL used to configure the pulverization system.
+ *
+ * ```kotlin
+ * val configurationResult = pulverization {
+ *   val device by logicDevice {
+ *     withBehaviour<DeviceBehaviour>() requires setOf(embeddedDevice, highCpu)
+ *     withSensors<DeviceSensors>() requires embeddedDevice
+ *     withCommunication<DeviceCommunication>() requires setOf(embeddedDevice, highCpu)
+ *   }
+ *   deployment(infrastructure, MqttProtocol()) {
+ *     device(device) {
+ *       DeviceBehaviour() startsOn server
+ *       DeviceSensors() startsOn smartphone
+ *       DeviceCommunication() startsOn smartphone
+ *     }
+ *   }
+ * }
+ * ```
  */
 fun pulverization(config: PulverizationScope.() -> Unit): Either<NonEmptyList<ConfigurationError>, PulvreaktConfiguration> =
     PulverizationScope().apply(config).generate()
