4cb6f5f8103c00b83ef28dae63ab7c32aa3f6567
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-01-04 09:53:18 +0100

    docs: improve documentation of the Host interface

--- core/src/commonMain/kotlin/it/unibo/pulvreakt/api/infrastructure/Host.kt
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/api/infrastructure/Host.kt
@@ -5,16 +5,27 @@ import arrow.core.nonEmptySetOf
 import it.unibo.pulvreakt.dsl.model.Capability
 
 /**
- * Represents the host of the available infrastructure.
+ * Models the concept of a **host** belonging to the infrastructure.
+ *
+ * A host in a pulverization system manage the execution of one or more components of one or more logical devices.
+ * The host is identified by its [hostname] and it has a set of [Capability] that defines the capabilities of the host.
+ *
+ * The capabilities are used to define the constraints on where a component can be deployed.
+ * If a component `A` requires the capabilities `c1` and `c2` to be deployed,
+ * the target host must have both the capabilities `c1` and `c2` to deploy the component `A`.
+ * In other words, a host must exhibit a super-set of the capabilities required by a component to be deployed.
  */
 sealed interface Host {
     /**
      * The hostname of the host.
+     * This is used to identify the host in the infrastructure and must be unique.
      */
     val hostname: String
 
     /**
      * The set of [Capability] of the host.
+     * This set defines the capabilities of the host and is used to define the constraints on where a component can be deployed.
+     * A host must exhibit at least one capability, as reported in the [NonEmptySet] type.
      */
     val capabilities: NonEmptySet<Capability>
 
