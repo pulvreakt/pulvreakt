7b7164f8e637b58307f7ff790147580a7e904986
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-01-18 08:24:33 +0100

    feat: modularization dsl

---
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/dsl/ModularizationDsl.kt
@@ -0,0 +1,3 @@
+package it.unibo.pulvreakt.modularization.dsl
+
+fun modularization(scope: ModularizationScope.() -> Unit): SystemModularized = TODO()
---
+++ core/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/dsl/ModularizationScope.kt
@@ -0,0 +1,5 @@
+package it.unibo.pulvreakt.modularization.dsl
+
+class ModularizationScope {
+
+}
