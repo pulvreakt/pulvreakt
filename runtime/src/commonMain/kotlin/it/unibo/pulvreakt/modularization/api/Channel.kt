b835453f31c96565511e45099f48906d2143f67c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-01-19 08:39:02 +0100

    refactor: minor refactoring

--- runtime/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/api/Channel.kt
+++ runtime/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/api/Channel.kt
@@ -2,7 +2,8 @@ package it.unibo.pulvreakt.modularization.api
 
 import kotlinx.coroutines.flow.Flow
 
-interface Channel<Message : Any> {
+interface Channel<Message : Any, Address : Any> {
+    fun setup(from: Address, to: Address)
     suspend fun send(message: Message)
     fun receive(): Flow<Message>
 }
--- runtime/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/api/DeploymentUnitImpl.kt
+++ runtime/src/commonMain/kotlin/it/unibo/pulvreakt/modularization/api/DeploymentUnitImpl.kt
@@ -1,8 +1,11 @@
 package it.unibo.pulvreakt.modularization.api
 
 import it.unibo.pulvreakt.modularization.dsl.data.ModularizedSystem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.KSerializer
 import org.kodein.di.DI
+import org.kodein.di.provider
 
 @PublishedApi
 internal class DeploymentUnitImpl<C : Capabilities>(
@@ -12,16 +15,26 @@ internal class DeploymentUnitImpl<C : Capabilities>(
     private val hostDiscover: HostDiscover,
     private val configuration: ModularizedSystem,
 ) : DeploymentUnit<C> {
+    private val registeredModules: MutableMap<Module<*, *, *>, Pair<KSerializer<*>, KSerializer<*>>> = mutableMapOf()
+    private val offloadedModules: MutableMap<Module<*, *, *>, Pair<KSerializer<*>, KSerializer<*>>> = mutableMapOf()
+    private val channelProvider by provider<Channel<ByteArray, String>>()
+    private val scope = CoroutineScope(Dispatchers.Default)
+
     override fun <Input : Any, Output : Any> registerModule(
         module: Module<*, Input, Output>,
         inputKSerializer: KSerializer<Input>,
         outputKSerializer: KSerializer<Output>,
     ) {
         module.setupInjector(di)
-        TODO()
+        when {
+            module.capabilities == runningHost.exposedCapabilities -> registeredModules += module to (inputKSerializer to outputKSerializer)
+            else -> offloadedModules += module to (inputKSerializer to outputKSerializer)
+        }
     }
 
     override suspend fun setup(): Outcome {
+        // Setup the channels
+        // Launch host discover
         TODO("Not yet implemented")
     }
 
@@ -30,6 +43,7 @@ internal class DeploymentUnitImpl<C : Capabilities>(
     }
 
     override suspend fun start(): Outcome {
+        // Start the scheduler reacting on incoming messages
         TODO("Not yet implemented")
     }
 
