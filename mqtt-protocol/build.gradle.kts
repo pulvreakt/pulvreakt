a305409909ead6fcf623525bbaacb14685ce29d3
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-05-21 15:18:42 +0200

    feat(mqtt-protocol): unified jvm and native

--- mqtt-protocol/build.gradle.kts
+++ mqtt-protocol/build.gradle.kts
@@ -8,95 +8,100 @@ kotlin {
                 implementation(project(":core"))
             }
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(rootProject.libs.bundles.kmqtt)
+        val multithreadMain by creating{
+            dependencies{
+                dependsOn(commonMain)
+                implementation(libs.bundles.kmqtt)
             }
         }
+
+        val jvmMain by getting {
+            dependencies {
+                dependsOn(multithreadMain)
+                api(libs.slf4j.simple)
+            }
+        }
+
         val jsMain by getting{
             dependencies {
                 implementation(rootProject.libs.kotlin.stdlib.js)
                 implementation(npm("mqtt", "5.5.3")) //todo use toml
             }
         }
-        val nativeMain by getting{
-            dependencies{
-                implementation(rootProject.libs.bundles.kmqtt)
-            }
-        }
+
         val linuxX64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-linux-x64.klib"))
             }
         }
 
         val tvosSimulatorArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-tvos-simulator-arm64.klib"))
             }
         }
 
         val tvosArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-tvos-arm64.klib"))
             }
         }
 
         val watchosSimulatorArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-watchos-simulator-arm64.klib"))
             }
         }
 
         val watchosArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-watchos-arm64.klib"))
             }
         }
 
         val iosX64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-ios-x64.klib"))
             }
         }
 
         val iosSimulatorArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-ios-simulator-arm64.klib"))
             }
         }
 
         val iosArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-ios-arm64.klib"))
             }
         }
 
         val macosArm64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-macos-arm64.klib"))
             }
         }
 
         val macosX64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-macos-x64.klib"))
             }
         }
 
         val mingwX64Main by getting {
             dependencies {
-                dependsOn(nativeMain)
+                dependsOn(multithreadMain)
                 implementation(files("native-libs/openssl-mingw-x64.klib"))
             }
         }
